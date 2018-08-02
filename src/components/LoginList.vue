e5012ffcb04370ea101cff39dfdfbe67ccda465f
Marvin W <git@larma.de>
2018-08-02 12:46:57 +0200

    Update: Rewrite in TypeScript, add Storybook, use npm to resolve dependencies

---
+++ src/components/LoginList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="login-list">
+        <div class="login-entry" v-for="login in logins" @click="loginSelected(login.id)">
+            <Login :user-friendly-id="login.userFriendlyId"
+                   :label="login.label"
+                   :id="login.id"
+                   :key="login.id"/>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Emit, Prop, Vue} from 'vue-property-decorator';
+    import Login from './Login.vue';
+
+    @Component({components: {Login}})
+    export default class LoginList extends Vue {
+        @Prop(Array) private logins!: Array<{ label: string, userFriendlyId: string, id: string }>;
+
+        @Emit()
+        // tslint:disable-next-line
+        private loginSelected(id: string) {
+        }
+    }
+</script>
+
+<style scoped>
+
+    .login-list .login-entry {
+        padding: 0.3rem;
+        border: 2px dashed transparent;
+        cursor: pointer;
+        transition: background .2s;
+    }
+
+    .login-list .login {
+        margin: 0 auto;
+    }
+
+    .login-list .login-entry:hover {
+        background-color: rgba(128, 128, 128, 0.1);
+    }
+</style>

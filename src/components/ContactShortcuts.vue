3bfb05cfa3311caace816d39e6dc477882c4a459
Sebastian <sebastian@nimiq.com>
2019-06-28 19:00:29 +0200

    add ContactShortcuts component

---
+++ src/components/ContactShortcuts.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="contact-shortcuts">
+        <div class="open-contacts" @click="contactsOpened">
+            <ContactsIcon/>
+            <div class="label">Contacts</div>
+        </div>
+        <div class="contacts">
+            <div href="javascript:void(0)" class="account-entry"
+                v-for="contact in filteredContacts" :key="contact.address"
+                @click="contactSelected(contact.address, contact.label)">
+                <Account
+                    layout="column"
+                    :address="contact.address"
+                    :label="contact.label" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
+import Account from './Account.vue';
+import { ContactsIcon } from './Icons';
+
+@Component({components: {Account, ContactsIcon}})
+    export default class ContactShortcuts extends Vue {
+        @Prop(Array) public contacts!: Array<{address: string, label: string}>;
+
+        private get filteredContacts() {
+            return this.contacts.slice(0, 3);
+        }
+
+        @Emit()
+        // tslint:disable-next-line no-empty
+        public contactsOpened() {}
+
+        @Emit()
+        // tslint:disable-next-line no-empty
+        public contactSelected(address: string, label: string) {}
+    }
+</script>
+
+<style>
+    .contact-shortcuts {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        font-size: 2rem;
+        width: 100%;
+    }
+
+    .contact-shortcuts .open-contacts {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-direction: column;
+        padding: 2rem 1rem;
+        width: 12.5rem;
+    }
+
+    .contact-shortcuts .open-contacts svg {
+        width: 5rem;
+        height: auto;
+        margin-bottom: 2.25rem;
+        color: var(--nimiq-blue);
+        opacity: .4;
+    }
+
+    .contact-shortcuts .contacts {
+        display: flex;
+        flex-direction: row;
+        width: calc(100% - 12.5rem - 1px);
+        border-left: 1px solid rgba(30,30,30, 0.1);
+    }
+
+    .contact-shortcuts .contacts .account-entry {
+        width: calc(100% / 3);
+    }
+
+    .contact-shortcuts .contacts .identicon {
+        width: 7rem;
+        height: auto;
+    }
+
+    .contact-shortcuts .contacts .label {
+        height: 1.5em;
+        overflow: hidden;
+        max-width: 100%;
+    }
+</style>

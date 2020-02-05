699dbeb576518c2ed2993dde4cc8ef4e7528c2db
Daniel <github.danimoh@neverbox.com>
2020-02-04 20:00:37 -0600

    stores/Transactions: simplify reactive update
    Simplify reactive update of the store by using Vue.set which
    probably is more performant than replacing the entries, garbage
    collecting and setting up all reactivity for the new object.

--- src/stores/Transactions.ts
+++ src/stores/Transactions.ts
@@ -1,7 +1,8 @@
+import Vue from 'vue';
+import { getHistoricExchangeRates } from '@nimiq/utils';
 import { createStore } from 'pinia'
 import { useFiatStore } from './Fiat';
 import { CryptoCurrency, FiatCurrency, FIAT_PRICE_UNAVAILABLE } from '../lib/Constants';
-import { getHistoricExchangeRates } from '@nimiq/utils';
 
 export type Transaction = ReturnType<import('@nimiq/core-web').Client.TransactionDetails["toPlain"]> & {
     fiatValue?: { [fiatCurrency: string]: number | typeof FIAT_PRICE_UNAVAILABLE | undefined },
@@ -44,22 +45,13 @@ export const useTransactionsStore = createStore({
 
             for (const tx of transactionsToUpdate) {
                 const exchangeRate = historicExchangeRates.get(tx.timestamp! * 1000);
-                tx.fiatValue = {
-                    ...tx.fiatValue,
-                    [fiatCurrency]: exchangeRate !== undefined
-                        ? Math.round(exchangeRate * tx.value) / 1e5 // rounded to 5 decimals
-                        : FIAT_PRICE_UNAVAILABLE,
-                };
+                // Set via Vue.set to let vue setup the reactivity. TODO this might be not necessary anymore with Vue3
+                if (!tx.fiatValue) Vue.set(tx, 'fiatValue', {});
+                Vue.set(tx.fiatValue!, fiatCurrency, exchangeRate !== undefined
+                    ? Math.round(exchangeRate * tx.value) / 1e5 // rounded to 5 decimals
+                    : FIAT_PRICE_UNAVAILABLE,
+                );
             }
-
-            // TODO: Simply assign transaction in Vue 3.
-            this.state.transactions = {
-                ...this.state.transactions,
-                ...transactionsToUpdate.reduce((state, tx) => ({
-                    ...state,
-                    [tx.transactionHash]: { ...tx }, // need to create a new object here for vue 2 to detect changes
-                }), {}),
-            };
         },
     },
 })

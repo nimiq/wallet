fbac956ef7c0f91b960539d1ceae2d56d0327e06
sisou <hello@soerenschwert.de>
2020-02-11 15:35:21 +0100

    Improve displayed decimals in PriceChart

--- src/components/PriceChart.vue
+++ src/components/PriceChart.vue
@@ -20,7 +20,7 @@
                     <!-- TODO: Adapt FiatAmount for this use case and use it here -->
                     <!-- FiatAmount cannot display more than 2 decimals, which is necessary for NIM. -->
                     <!-- <FiatAmount :amount="endPrice" :currency="fiatCurrency"/> -->
-                    <div v-if="endPrice !== undefined">{{ endPrice.toFixed(4) }} {{ fiatSymbol }}</div>
+                    <div v-if="displayPrice !== undefined">{{ displayPrice }} {{ fiatSymbol }}</div>
                 </transition>
                 <transition name="fade">
                     <div v-if="priceChange !== undefined" class="change" :class="priceChangeClass">
@@ -38,6 +38,20 @@ import { CurrencyInfo, getHistoricExchangeRates } from '@nimiq/utils';
 // import { FiatAmount } from '@nimiq/vue-components';
 import { CryptoCurrency } from '../lib/Constants';
 import { useFiatStore } from '../stores/Fiat';
+
+function roundToSignificant(number: number, places = 1) {
+  const roundingFactor = number < 0.001
+    ? 10 ** (3 + places)
+    : number < 0.01
+        ? 10 ** (2 + places)
+        : number < 0.1
+            ? 10 ** (1 + places)
+            : number < 10
+                ? 10 ** (places)
+                : 10 ** (places - 1);
+
+    return Math.round(number * roundingFactor) / roundingFactor;
+}
 
 export default createComponent({
     name: 'price-chart',
@@ -90,6 +104,7 @@ export default createComponent({
                 : priceChange.value < 0
                     ? 'negative'
                     : 'none');
+        const displayPrice = computed(() => endPrice.value && roundToSignificant(endPrice.value, 3));
 
         // Calculate path
         const path = computed(() => {
@@ -205,7 +220,7 @@ export default createComponent({
             strokeWidth,
             viewBox,
             path,
-            endPrice,
+            displayPrice,
             fiatSymbol,
             history,
             priceChange,

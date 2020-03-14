dea2839642c2599d3e3d98902689a511820b709d
sisou <hello@soerenschwert.de>
2020-03-14 16:04:08 +0100

    Fix service worker updating

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -2,6 +2,14 @@
 
 import { register } from 'register-service-worker';
 
+let refreshing = false;
+navigator.serviceWorker.addEventListener('controllerchange', () => {
+    console.debug('NEW SERVICEWORKER ACTIVATED, RELOADING!');
+    if (refreshing) return;
+    refreshing = true;
+    setTimeout(() => window.location.reload(true), 1000);
+});
+
 if (process.env.NODE_ENV === 'production') {
     register(`${process.env.BASE_URL}service-worker.js`, {
         ready() {
@@ -19,11 +27,17 @@ if (process.env.NODE_ENV === 'production') {
         updatefound() {
             console.log('New content is downloading.');
         },
-        updated() {
+        updated(registration) {
             console.log('New content is available; please refresh.');
+
+            if (!registration || !registration.waiting) {
+                console.warn('SW fired update(), but without reference to SW', registration);
+                return;
+            }
+
             // eslint-disable-next-line no-alert
             if (window.confirm('A new version of the Wallet is available.\nReload to update?')) {
-                window.location.reload(true);
+                registration.waiting.postMessage({ type: 'SKIP_WAITING' });
             }
         },
         offline() {

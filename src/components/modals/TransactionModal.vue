4ed5624424be924b29efda3b47c3619c19af6eaa
sisou <hello@soerenschwert.de>
2020-06-02 08:53:46 +0200

    Small missing addition for last commit

--- src/components/modals/TransactionModal.vue
+++ src/components/modals/TransactionModal.vue
@@ -58,13 +58,13 @@
                 {{ state === TransactionState.EXPIRED ? $t('Expired') : $t('Failed') }}
             </span>
             <span v-else slot="more" :class="isIncoming ? 'nq-green' : 'opacity-60'">
-                <i18n v-if="isIncoming" path="received at {dateAndTime}">
-                    <template #dateAndTime>
+                <i18n v-if="isIncoming" path="received at {dateAndTime}" :tag="false">
+                    <template v-slot:dateAndTime>
                         {{ datum }} <strong>&middot;</strong> {{ time }}
                     </template>
                 </i18n>
-                <i18n v-else path="sent at {dateAndTime}">
-                    <template #dateAndTime>
+                <i18n v-else path="sent at {dateAndTime}" :tag="false">
+                    <template v-slot:dateAndTime>
                         {{ datum }} <strong>&middot;</strong> {{ time }}
                     </template>
                 </i18n>

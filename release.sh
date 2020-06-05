f4f01064b15d1805ed7423b1fe2ba21c7bf2b260
sisou <hello@soerenschwert.de>
2020-06-05 12:47:50 +0200

    Add release script

---
+++ release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Make sure a tag name was provided
+tag=$1
+
+if [ -z "$tag" ]; then
+    echo "ERROR: Must provide a tag name."
+    exit 1
+fi
+
+# Sync language file
+yarn i18n:sync
+
+if [[ `git status --porcelain` ]]; then
+    echo "ERROR: The repository has changes. Commit them first, then run again."
+    exit 1
+fi
+
+# Build Vue app
+yarn build
+
+# Tag current commit
+git tag -a -s $1
+
+# Push commit and tag to origin
+git push && git push --tags
+
+# Done
+echo "FINISHED - The app was built into the ./dist folder"

052c900586fbcae17b5889e4a9e0b5386c7c9e99
sisou <hello@soerenschwert.de>
2020-06-23 17:09:36 +0200

    Enable editing of contacts in ContactBook

--- src/components/ContactBook.vue
+++ src/components/ContactBook.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="contact-book flex-column">
+    <div class="contact-book flex-column" :class="{ editing }">
         <div class="list flex-column">
             <AddressListItem
                 v-for="addressInfo in (showAllOwnAddresses ? ownAddressInfos : ownAddressInfos.slice(0, 2))"
@@ -7,9 +7,9 @@
                 :addressInfo="addressInfo"
                 @click="$emit('contact-selected', addressInfo, RecipientType.OWN_ADDRESS)"
             />
-            <button v-if="ownAddressInfos.length > 2"
+            <button v-if="ownAddressInfos.length > 2 && !showAllOwnAddresses"
                 class="nq-button-s show-own-addresses-button"
-                @click="showAllOwnAddresses = !showAllOwnAddresses">
+                @click="showAllOwnAddresses = true">
                 {{ $t('Show all my addresses') }}
             </button>
             <div v-if="ownAddressInfos.length" class="separator"></div>
@@ -17,22 +17,37 @@
                 v-for="contact in contacts"
                 :key="contact.address"
                 class="reset contact-button flex-row"
-                @click="$emit('contact-selected', contact, RecipientType.CONTACT)"
+                @click="!editing && $emit('contact-selected', contact, RecipientType.CONTACT)"
+                :tabindex="editing ? -1 : 0"
             >
                 <Identicon :address="contact.address"/>
-                <label>{{ contact.label }}</label>
+                <label v-if="!editing">{{ contact.label }}</label>
+                <LabelInput v-else
+                    :value="contact.label"
+                    :placeholder="$t('Name your contact')"
+                    @input="onInput(contact.address, $event)"/>
+                <div class="flex-grow"></div>
                 <ShortAddress :address="contact.address"/>
+                <button v-if="editing" class="reset delete-button" @click="setContact(contact.address, '')">
+                    <TrashIcon/>
+                </button>
             </button>
         </div>
-        <button class="nq-button-s edit-button">{{ $t('Edit contacts') }}</button>
+        <button v-if="!editing" class="nq-button-s edit-button" @click="editing = true" key="edit">
+            {{ $t('Edit contacts') }}
+        </button>
+        <button v-else class="nq-button-pill light-blue edit-button" @click="editing = false" key="done">
+            {{ $t('Done') }}
+        </button>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed, ref } from '@vue/composition-api';
-import { Identicon } from '@nimiq/vue-components';
+import { Identicon, LabelInput } from '@nimiq/vue-components';
 import AddressListItem from './AddressListItem.vue';
 import ShortAddress from './ShortAddress.vue';
+import TrashIcon from './icons/TrashIcon.vue';
 import { RecipientType } from './modals/SendModal.vue';
 import { useContactsStore } from '../stores/Contacts';
 import { useAddressStore } from '../stores/Address';
@@ -51,17 +66,31 @@ export default defineComponent({
 
         const showAllOwnAddresses = ref(false);
 
+        const editing = ref(false);
+
+        function onInput(address: string, label: string) {
+            // Don't delete contacts when label is cleared, as that would remove it from the list
+            if (!label) return;
+
+            setContact(address, label);
+        }
+
         return {
             contacts,
             ownAddressInfos,
             showAllOwnAddresses,
             RecipientType,
+            editing,
+            onInput,
+            setContact,
         };
     },
     components: {
         Identicon,
         AddressListItem,
         ShortAddress,
+        LabelInput,
+        TrashIcon,
     },
 });
 </script>
@@ -89,12 +118,19 @@ export default defineComponent({
 .address-button,
 .contact-button {
     width: 100%;
-    transition: background 0.2s var(--nimiq-ease);
 
-    &:hover,
-    &:focus {
-        background: var(--nimiq-highlight-bg);
-    }
+    transition: background 0.2s var(--nimiq-ease);
+}
+
+.address-button:hover,
+.address-button:focus,
+.contact-book:not(.editing) .contact-button:hover,
+.contact-book:not(.editing) .contact-button:focus {
+    background: var(--nimiq-highlight-bg);
+}
+
+.contact-book.editing .contact-button {
+    cursor: auto;
 }
 
 .show-own-addresses-button {
@@ -119,30 +155,74 @@ export default defineComponent({
         flex-shrink: 0;
     }
 
-    label {
+    label,
+    .label-input {
         margin: 0 2rem;
         font-weight: 600;
-        cursor: pointer;
-        flex-grow: 1;
+    }
+
+    .label-input {
+        margin-left: 1.25rem;
+
+        /deep/ .width-finder {
+            padding: 0 0.75rem;
+        }
+
+        /deep/ input {
+            padding: 0.625rem 0.75rem;
+            padding-right: 0;
+        }
     }
 
     .short-address {
         opacity: 0.5;
         flex-shrink: 0;
     }
+
+    .delete-button {
+        padding: 1rem;
+        border-radius: 0.5rem;
+        margin: 0 -1rem 0 1rem;
+        flex-shrink: 0;
+
+        transition:
+            background 0.2s var(--nimiq-ease),
+            color 0.2s var(--nimiq-ease);
+
+        svg {
+            opacity: 0.4;
+            display: block;
+
+            transition: opacity 0.2s var(--nimiq-ease);
+        }
+
+        &:hover,
+        &:focus {
+            background: rgba(216, 65, 51, 0.12); // Based on Nimiq Red
+            color: var(--nimiq-red);
+
+            svg {
+                opacity: 1;
+            }
+        }
+    }
 }
 
 .edit-button {
     align-self: center;
-    margin-right: 2rem;
 }
 
 @media (max-width: 700px) { // Full mobile breakpoint
     .contact-button {
         background: none !important;
 
-        label {
+        label,
+        .label-input {
             margin: 0 1.5rem;
+        }
+
+        .label-input {
+            margin-left: 0.75rem;
         }
     }
 }
---
+++ src/components/icons/TrashIcon.vue
@@ -0,0 +1,13 @@
+<template functional>
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g>
+      <path d="M2.75 3.75h10.5V14c0 .69-.56 1.25-1.25 1.25H4c-.69 0-1.25-.56-1.25-1.25V3.75z" stroke="currentColor" stroke-width="1.5"/>
+      <path d="M.75 3.74h14.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <mask id="a" fill="#fff">
+        <path d="M4.5 1a1 1 0 011-1h5a1 1 0 011 1v3.5h-7V1z"/>
+      </mask>
+      <path d="M4.5 1a1 1 0 011-1h5a1 1 0 011 1v3.5h-7V1z" stroke="currentColor" stroke-width="3" mask="url(#a)"/>
+      <path stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" d="M6.25 7.75v3.5M9.75 7.75v3.5"/>
+    </g>
+  </svg>
+</template>

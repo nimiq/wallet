772ff6a5dbb7f3167c0549ccefbd4b2b1048e58c
Mathéo <matheo.raveux@gmail.com>
2020-06-26 22:03:12 +0200

    Adapt Total Balance font-size to available column width, so it doesn’t overflow and gets cut off

--- src/components/AccountBalance.vue
+++ src/components/AccountBalance.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="account-balance">
         <h2 class="nq-label">{{ $t('Total Balance') }}</h2>
-        <div class="fiat-amount">
-            <FiatConvertedAmount :amount="accountBalance" />
+        <div class="fiat-amount" ref="$fiatAmountContainer">
+            <FiatConvertedAmount
+                :amount="1089716251785456"
+                ref="$fiatAmount"
+                :style="{ fontSize: `${fiatAmountFontSize}rem` }"
+            />
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
-import { useAddressStore } from '../stores/Address';
+import Vue from 'vue';
+import { defineComponent, ref, onBeforeUnmount, onMounted } from '@vue/composition-api';
 import FiatConvertedAmount from './FiatConvertedAmount.vue';
+import { useAddressStore } from '../stores/Address';
 // re add <BalanceDistribution /> once Bitcoin is avalable
 // import BalanceDistribution from './BalanceDistribution.vue';
 
@@ -18,8 +23,32 @@ export default defineComponent({
     setup() {
         const { accountBalance } = useAddressStore();
 
+        const $fiatAmountContainer = ref<HTMLDivElement>(null);
+        const $fiatAmount = ref<Vue>(null);
+        const fiatAmountDefautFontSize = 7; // the default desktop font-size of .fiat-amount
+        const fiatAmountFontSize = ref(fiatAmountDefautFontSize);
+
+        async function updateFontSize() {
+            await Vue.nextTick();
+
+            const availableWidth = $fiatAmountContainer.value!.offsetWidth;
+            const referenceWidth = ($fiatAmount.value!.$el as HTMLElement).offsetWidth;
+            const scaleFactor = Math.round((availableWidth / referenceWidth) * 100) / 100;
+
+            if (scaleFactor > 1.02 || scaleFactor < 0.98) { // needed for safari
+                fiatAmountFontSize.value = Math.min(fiatAmountDefautFontSize, fiatAmountFontSize.value * scaleFactor);
+            }
+        }
+
+        window.addEventListener('resize', updateFontSize);
+        onBeforeUnmount(() => window.removeEventListener('resize', updateFontSize));
+        onMounted(() => updateFontSize());
+
         return {
             accountBalance,
+            $fiatAmount,
+            $fiatAmountContainer,
+            fiatAmountFontSize,
         };
     },
     components: {

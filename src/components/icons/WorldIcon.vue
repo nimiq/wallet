ab8024458a87d2e8c6aaa8b3a2be0090d87c0a85
sisou <hello@soerenschwert.de>
2020-06-29 13:29:31 +0200

    Make WorldIcon adapt to parent size

--- src/components/icons/WorldIcon.vue
+++ src/components/icons/WorldIcon.vue
@@ -6,11 +6,11 @@
 .world-icon,
 .world-icon::before,
 .world-icon::after {
-    width: 22px;
-    height: 22px;
+    width: 100%;
+    height: 100%;
     background-image: url('../../assets/consensus-sprite.svg');
     background-repeat: repeat-x;
-    background-size: 220px 66px;
+    background-size: 1000% 300%;
 }
 
 .world-icon::before,
@@ -29,12 +29,12 @@
 
 .world-icon {
     position: relative;
-    background-position: 0 -22px;
+    background-position: 0 50%;
     animation: center 4.6s steps(10) infinite;
 }
 
 .world-icon::after {
-    background-position: 0 -44px;
+    background-position: 0 100%;
     animation: bottom 4.6s steps(10) infinite -0.4s;
 }
 
@@ -75,63 +75,63 @@
  */
 
 @keyframes top {
-    0%           { background-position: 0 0; }
+    0%      { background-position: 0 0; }
     /* +10 frames still = 10th frame */
-    8.69565217%  { background-position: 0 0; }
+    8.695%  { background-position: 0 0; }
     /* +4 x rightround = +40 frames = 50th frame */
-    17.39130435% { background-position: calc(-10 * 22px) 0; }
-    26.08695652% { background-position: calc(-20 * 22px) 0; }
-    34.7826087%  { background-position: calc(-30 * 22px) 0; }
-    43.47826087% { background-position: calc(-40 * 22px) 0; }
+    17.391% { background-position: calc(-10 * 100%) 0; }
+    26.086% { background-position: calc(-20 * 100%) 0; }
+    34.782% { background-position: calc(-30 * 100%) 0; }
+    43.478% { background-position: calc(-40 * 100%) 0; }
     /* +5 frames still = 55th frame */
-    47.82608696% { background-position: calc(-40 * 22px) 0; }
+    47.826% { background-position: calc(-40 * 100%) 0; }
     /* +6 x leftround = +60 frames = 115th frame */
-    56.52173913% { background-position: calc(-30 * 22px) 0; }
-    65.2173913%  { background-position: calc(-20 * 22px) 0; }
-    73.91304348% { background-position: calc(-10 * 22px) 0; }
-    82.60869565% { background-position: 0 0; }
-    91.30434783% { background-position: calc( 10 * 22px) 0; }
-    100%         { background-position: calc( 20 * 22px) 0; }
+    56.521% { background-position: calc(-30 * 100%) 0; }
+    65.217% { background-position: calc(-20 * 100%) 0; }
+    73.913% { background-position: calc(-10 * 100%) 0; }
+    82.608% { background-position: 0 0; }
+    91.304% { background-position: calc( 10 * 100%) 0; }
+    100%    { background-position: calc( 20 * 100%) 0; }
 }
 
 @keyframes center {
-    0% { background-position: 0 -22px; }
+    0%      { background-position: 0 50%; }
     /* +1 x leftround = +10 frames = 10th frame */
-    8.695652174% { background-position: calc( 10 * 22px) -22px; }
+    8.695%  { background-position: calc( 10 * 100%) 50%; }
     /* +15 frames still = 25th frame */
-    21.73913043% { background-position: calc( 10 * 22px) -22px; }
+    21.739% { background-position: calc( 10 * 100%) 50%; }
     /* +5 x rightround = +50 frames = 75th frame */
-    30.43478261% { background-position: 0 -22px; }
-    39.13043478% { background-position: calc(-10 * 22px) -22px; }
-    47.82608696% { background-position: calc(-20 * 22px) -22px; }
-    56.52173913% { background-position: calc(-30 * 22px) -22px; }
-    65.2173913%  { background-position: calc(-40 * 22px) -22px; }
+    30.434% { background-position: 0 50%; }
+    39.130% { background-position: calc(-10 * 100%) 50%; }
+    47.826% { background-position: calc(-20 * 100%) 50%; }
+    56.521% { background-position: calc(-30 * 100%) 50%; }
+    65.217% { background-position: calc(-40 * 100%) 50%; }
     /* +10 frames still = 85th frame */
-    73.91304348% { background-position: calc(-40 * 22px) -22px; }
+    73.913% { background-position: calc(-40 * 100%) 50%; }
     /* +3 x leftround = +30 frames = 115th frame */
-    82.60869565% { background-position: calc(-30 * 22px) -22px; }
-    91.30434783% { background-position: calc(-20 * 22px) -22px; }
-    100%         { background-position: calc(-10 * 22px) -22px; }
+    82.608% { background-position: calc(-30 * 100%) 50%; }
+    91.304% { background-position: calc(-20 * 100%) 50%; }
+    100%    { background-position: calc(-10 * 100%) 50%; }
 }
 
 /* Same as top, but left and right switched, and started 10 frames early (-0.4s animation delay) */
 @keyframes bottom {
-    0%           { background-position: 0 -44px; }
+    0%      { background-position: 0 100%; }
     /* +10 frames still = 10th frame */
-    8.69565217%  { background-position: 0 -44px; }
+    8.695%  { background-position: 0 100%; }
     /* +4 x leftround = +40 frames = 50th frame */
-    17.39130435% { background-position: calc( 10 * 22px) -44px; }
-    26.08695652% { background-position: calc( 20 * 22px) -44px; }
-    34.7826087%  { background-position: calc( 30 * 22px) -44px; }
-    43.47826087% { background-position: calc( 40 * 22px) -44px; }
+    17.391% { background-position: calc( 10 * 100%) 100%; }
+    26.086% { background-position: calc( 20 * 100%) 100%; }
+    34.782% { background-position: calc( 30 * 100%) 100%; }
+    43.478% { background-position: calc( 40 * 100%) 100%; }
     /* +5 frames still = 55th frame */
-    47.82608696% { background-position: calc( 40 * 22px) -44px; }
+    47.826% { background-position: calc( 40 * 100%) 100%; }
     /* +6 x rightround = +60 frames = 115th frame */
-    56.52173913% { background-position: calc( 30 * 22px) -44px; }
-    65.2173913%  { background-position: calc( 20 * 22px) -44px; }
-    73.91304348% { background-position: calc( 10 * 22px) -44px; }
-    82.60869565% { background-position: 0 -44px; }
-    91.30434783% { background-position: calc(-10 * 22px) -44px; }
-    100%         { background-position: calc(-20 * 22px) -44px; }
+    56.521% { background-position: calc( 30 * 100%) 100%; }
+    65.217% { background-position: calc( 20 * 100%) 100%; }
+    73.913% { background-position: calc( 10 * 100%) 100%; }
+    82.608% { background-position: 0 100%; }
+    91.304% { background-position: calc(-10 * 100%) 100%; }
+    100%    { background-position: calc(-20 * 100%) 100%; }
 }
 </style>

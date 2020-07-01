f44981ddf48083e4020d1efc62b250ca3959bc12
Mathéo <matheo.raveux@gmail.com>
2020-07-01 12:11:12 +0200

    IOS fix loader

--- src/App.vue
+++ src/App.vue
@@ -186,6 +186,10 @@ export default defineComponent({
         position: fixed;
         width: 100%;
         height: 100%;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
         justify-content: center;
         align-items: center;
         background: var(--nimiq-gray);

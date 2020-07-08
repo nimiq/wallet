9d094d8f663cb9afcd2a0dfcb49cafa8e1545657
sisou <hello@soerenschwert.de>
2020-07-08 16:13:43 +0200

    Enable dismissing the AnnouncementBox

--- src/components/AnnouncementBox.vue
+++ src/components/AnnouncementBox.vue
@@ -1,22 +1,27 @@
 <template>
-    <div v-if="text()" class="announcement-box nq-green-bg">
+    <div v-if="!wasDismissed && text()" class="announcement-box nq-green-bg">
         <p>{{ text() }}</p>
         <BlueLink v-if="typeof action === 'string'" :href="action" target="_blank">{{ cta() }}</BlueLink>
         <button v-else class="reset action flex-row" @click="action">{{ cta() }}<ArrowRightSmallIcon/></button>
+        <CrossCloseButton class="top-right" @click="close"/>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, ref } from '@vue/composition-api';
 import { ArrowRightSmallIcon } from '@nimiq/vue-components';
 import { LocaleMessage } from 'vue-i18n';
 import BlueLink from './BlueLink.vue';
+import CrossCloseButton from './CrossCloseButton.vue';
+
+const STORAGE_KEY = 'announcement-box-dismissed';
 
 export default defineComponent({
     setup(props, context) {
         let text: () => LocaleMessage = () => '';
         let cta: () => LocaleMessage = () => '';
         let action: string | Function = '';
+        let storageKey = ''; // Used to identify if the box has been dismissed yet
 
         // // Dark Mode
         // text = context.root.$t('View your Wallet in Dark Mode!');
@@ -27,22 +32,34 @@ export default defineComponent({
         text = () => context.root.$t('Got feedback? Found a bug?');
         cta = () => context.root.$t('Tell us here');
         action = 'https://forum.nimiq.community/t/new-wallet-feedback-thread/845';
+        storageKey = 'feedback-and-bugs';
+
+        const wasDismissed = ref(window.localStorage.getItem(STORAGE_KEY) === storageKey);
+
+        function close() {
+            wasDismissed.value = true;
+            window.localStorage.setItem(STORAGE_KEY, storageKey);
+        }
 
         return {
             text,
             cta,
             action,
+            wasDismissed,
+            close,
         };
     },
     components: {
         BlueLink,
         ArrowRightSmallIcon,
+        CrossCloseButton,
     },
 });
 </script>
 
 <style lang="scss" scoped>
 .announcement-box {
+    position: relative;
     padding: 1.5rem;
     border-radius: 0.75rem;
 }
---
+++ src/components/CrossCloseButton.vue
@@ -0,0 +1,38 @@
+<template>
+    <button class="cross-close-button reset" v-on="$listeners">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12">
+            <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2">
+                <line class="cls-1" x1="1" y1="1" x2="11" y2="11"/>
+                <line class="cls-1" x1="11" y1="1" x2="1" y2="11"/>
+            </g>
+        </svg>
+    </button>
+</template>
+
+<style lang="scss" scoped>
+    .cross-close-button {
+        font-size: 1.5rem;
+        padding: 0.5rem;
+        border-radius: 50%
+    }
+
+    svg {
+        height: 1em;
+        width: 1em;
+        display: block;
+        opacity: 0.7;
+        transition: opacity .3s var(--nimiq-ease);
+    }
+
+    button:hover svg,
+    button:active svg,
+    button:focus svg {
+        opacity: 1;
+    }
+
+    .cross-close-button.top-right {
+        position: absolute;
+        top: 1rem;
+        right: 1rem;
+    }
+</style>

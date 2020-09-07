30883f9f102d0ed88244df19da2fc771619f2aaf
Mathéo <matheo.raveux@gmail.com>
2020-09-07 13:03:41 +0200

    BtcSendModal: LabelInput tooltip

--- src/components/BtcLabelInput.vue
+++ src/components/BtcLabelInput.vue
@@ -2,6 +2,10 @@
     <div class="btc-label-input" :class="{disabled}">
         <LabelInput v-bind="$attrs" v-on="$listeners" v-model="localValue" :disabled="disabled"/>
         <Avatar :label="localValue"/>
+        <div class="blue-tooltip">
+            <p>{{ $t('Add a label to quickly find the transaction in your history.') }}</p>
+            <p>{{ $t('With Bitcoin, there are no contacts, since addresses are only used once.') }}</p>
+        </div>
     </div>
 </template>
 
@@ -83,6 +87,61 @@ export default defineComponent({
             /deep/ input {
                 color: var(--text-50);
             }
+        }
+
+        .blue-tooltip {
+            --width: 28rem;
+            width: var(--width);
+            padding: 1.5rem;
+            position: absolute;
+            top: 100%;
+            left: calc(0px - var(--width) - 2rem);
+            z-index: 10;
+            font-size: var(--small-size);
+            line-height: 140%;
+            text-align: left;
+            background: var(--nimiq-light-blue-bg);
+            color: white;
+            border-radius: 0.5rem;
+
+            opacity: 0;
+            transform: translateY(-50%) translateX(-.5rem);
+
+            transition: {
+                property: opacity, transform;
+                duration: 200ms;
+            }
+
+            p:first-child {
+                margin-top: 0;
+            }
+            p:last-child {
+                margin-bottom: 0;
+            }
+
+            &::after {
+                content: '';
+                display: block;
+                position: absolute;
+                width: 2.75rem;
+                height: 2.5rem;
+                top: 30%;
+                right: 0;
+                // eslint-disable-next-line max-len
+                mask-image: url('data:image/svg+xml,<svg viewBox="0 0 18 16" xmlns="http://www.w3.org/2000/svg"><path d="M9 7.12c-.47 0-.93.2-1.23.64L3.2 14.29A4 4 0 0 1 0 16h18a4 4 0 0 1-3.2-1.7l-4.57-6.54c-.3-.43-.76-.64-1.23-.64z" fill="white"/></svg>');
+                mask-size: contain;
+                z-index: 10;
+                background: #1A6AD2;
+                transform: translateY(-50%) translateX(calc(100% - 0.25rem)) rotate(90deg);
+            }
+        }
+
+        .label-input:focus-within ~ .blue-tooltip {
+            opacity: 1;
+            pointer-events: inherit;
+            user-select: none;
+
+            transform: translateY(-50%) translateX(0);
         }
     }
 </style>

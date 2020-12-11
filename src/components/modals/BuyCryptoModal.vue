a53002aa83534d8671c10a43c8d79a7cbf965551
Mathéo <matheo.raveux@gmail.com>
2020-12-11 16:37:20 +0100

    remove test values

--- src/components/modals/BuyCryptoModal.vue
+++ src/components/modals/BuyCryptoModal.vue
@@ -158,8 +158,8 @@ enum Pages {
 
 export default defineComponent({
     setup(/* props, context */) {
-        const page = ref(Pages.SETUP_BUY); // For testing, you can set this to Pages.SETUP_BUY
-        const selectedBank = ref<null | BankInfos>({ name: 'Berliner Sparkasse', type: 'sepa-instant-full-support' });
+        const page = ref(Pages.WELCOME); // For testing, you can set this to Pages.SETUP_BUY
+        const selectedBank = ref<null | BankInfos>(null);
         // & this by a fake bank. ex: { name: 'Berliner Sparkasse', type: 'sepa-instant-full-support' }
         const addressListOpened = ref(false);
         const { addressInfos, activeAddressInfo } = useAddressStore();

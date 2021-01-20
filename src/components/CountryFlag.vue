c795c772ec18758e4dcc46ad9f81217a3a6111aa
Sören <hello@soerenschwert.de>
2021-01-20 16:30:33 +0100

    Simplify CountryFlag component

--- src/components/CountryFlag.vue
+++ src/components/CountryFlag.vue
@@ -1,42 +1,13 @@
 <template>
-    <div class="country-flag">
-        <img :src="path" v-if="imageIsVisible"/>
-        <span class="placeholder" v-else>{{ code }}</span>
-    </div>
+    <img class="country-flag" :src="require(`../assets/flags/square/${code}.svg`)" :alt="code"/>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch } from '@vue/composition-api';
-import Avatar from './Avatar.vue';
+import { defineComponent } from '@vue/composition-api';
 
 export default defineComponent({
     props: {
         code: String,
-    },
-    setup(props) {
-        const imageIsVisible = ref(false);
-        const path = ref<string>('');
-
-        watch(() => props.code, (newCode, oldCode) => {
-            if (oldCode === newCode) return;
-
-            import(`../assets/flags/square/${props.code}.svg`)
-                .then((data) => {
-                    path.value = data.default;
-                    imageIsVisible.value = true;
-                })
-                .catch(() => {
-                    imageIsVisible.value = false;
-                });
-        });
-
-        return {
-            path,
-            imageIsVisible,
-        };
-    },
-    components: {
-        Avatar,
     },
 });
 </script>
@@ -47,23 +18,7 @@ export default defineComponent({
     width: var(--size);
     height: var(--size);
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
     overflow: hidden;
-    border-radius: calc(var(--size) / 2);
-
-    img {
-        height: 100%;
-        width: auto;
-    }
-
-    .placeholder {
-        font-size: var(--small-size);
-        background: var(--nimiq-blue-bg);
-        color: white;
-        width: 100%;
-        line-height: var(--size);
-    }
+    border-radius: 50%;
 }
 </style>

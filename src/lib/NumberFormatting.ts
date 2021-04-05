bd241acc3321542c5349d0a0e3a45bb5450875e3
Sabin <neokrept@gmail.com>
2021-04-06 01:25:56 +0300

    2nd Step draft

--- src/lib/NumberFormatting.ts
+++ src/lib/NumberFormatting.ts
@@ -45,3 +45,56 @@ export function calculateDisplayedDecimals(amount: number | null, currency: Cryp
     if (amount < 10 * 1e5) return Math.max(decimals.value, 1);
     return decimals.value;
 }
+
+export function numberToLiteral(n: number): String {
+    //https://stackoverflow.com/questions/5529934/javascript-numbers-to-words
+    var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    
+    function convert_millions(num: number): String {
+      if (num >= 1000000) {
+        return convert_millions(Math.floor(num / 1000000)) + " million " + convert_thousands(num % 1000000);
+      } else {
+        return convert_thousands(num);
+      }
+    }
+    
+    function convert_thousands(num: number): String {
+      if (num >= 1000) {
+        return convert_hundreds(Math.floor(num / 1000)) + " thousand " + convert_hundreds(num % 1000);
+      } else {
+        return convert_hundreds(num);
+      }
+    }
+    
+    function convert_hundreds(num: number): String {
+      if (num > 99) {
+        return ones[Math.floor(num / 100)] + " hundred " + convert_tens(num % 100);
+      } else {
+        return convert_tens(num);
+      }
+    }
+    
+    function convert_tens(num: number): String {
+        if (num < 10) return ones[num];
+        else if (num >= 10 && num < 20) return teens[num - 10];
+        else {
+            return tens[Math.floor(num / 10)] + " " + ones[num % 10];
+        }
+    }
+    
+    if (n == 0)
+        return "zero";
+    return convert_millions(n);
+}
+
+export function numberToLiteralTimes(n: number): String {
+    const timesTable = ["", "once", "twice", "thrice"];
+    n = parseInt(n.toString());
+    if (n <= 0)
+        throw new Error("Invalid Input! Times number must be positive >= 1!");
+    if (n < timesTable.length)
+        return timesTable[n];
+    return numberToLiteral(n) + " times";
+}

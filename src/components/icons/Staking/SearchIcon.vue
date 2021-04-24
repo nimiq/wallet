701b298977fe8f5c37e3cf19bf5843709c281dcc
Sabin <neokrept@gmail.com>
2021-04-24 12:17:28 +0300

    Update to search + star icons and misc styling fixes

--- src/components/icons/Staking/SearchIcon.vue
+++ src/components/icons/Staking/SearchIcon.vue
@@ -1,8 +1,6 @@
 <template functional>
-    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g opacity="0.4">
-            <circle cx="5.60813" cy="5.60716" r="4.60813" stroke="#1F2348" stroke-width="2"/>
-            <path d="M12.291 13.7081C12.6815 14.0986 13.3147 14.0986 13.7052 13.7081C14.0957 13.3176 14.0957 12.6844 13.7052 12.2939L12.291 13.7081ZM7.66399 9.08113L12.291 13.7081L13.7052 12.2939L9.0782 7.66692L7.66399 9.08113Z" fill="#1F2348"/>
-        </g>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
+        <circle cx="5.25" cy="5.25" r="4.5" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/>
+        <line x1="13.25" y1="13.25" x2="8.75" y2="8.75" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/>
     </svg>
 </template>

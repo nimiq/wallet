e40e9258e11908a1bc0563a103d26018cb4c088c
Sabin <neokrept@gmail.com>
2021-04-27 10:58:05 +0300

    Amount slider POC

---
+++ src/components/icons/Staking/OneLeafStakingIcon.vue
@@ -0,0 +1,6 @@
+<template functional>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+        <path d="M11.75,5.25q0,4.5-4.5,4.5C7.25,5.5,8.5,5.25,11.75,5.25Z" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/>
+        <path d="M5.25,5.75a10.61,10.61,0,0,1,2,6" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/>
+    </svg>
+</template>

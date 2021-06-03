61d12dcbb733563acc42155d9b15799ceb7da897
Cirimpei Sabin <neokrept@gmail.com>
2021-06-02 18:22:13 -0600

    Mockpare

--- src/components/layouts/AddressOverview.vue
+++ src/components/layouts/AddressOverview.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="address-overview"
         :class="{ 'no-accounts flex-column': !activeAddressInfo && activeCurrency !== CryptoCurrency.BTC }">
-        <span class="feature-bar dev-bar">
-            <FeatureToggler featureName="necklace" />
-            <FeatureToggler featureName="stackattack" />
+        <span class="dev-bar">
+            <span class="feature-bar">
+                <FeatureToggler featureName="necklace" />
+                <FeatureToggler featureName="stackattack" />
+            </span>
+            <span class="mockpare">
+                <Mockpare />
+            </span>
         </span>
         <template v-if="activeAddressInfo || activeCurrency === CryptoCurrency.BTC">
             <div class="actions-mobile flex-row">
@@ -223,6 +228,7 @@ import RenameIcon from '../icons/AccountMenu/RenameIcon.vue';
 import RefreshIcon from '../icons/RefreshIcon.vue';
 import StakingHeroIcon from '../icons/Staking/StakingHeroIcon.vue';
 import FeatureToggler from '../widgets/FeatureToggler.vue';
+import Mockpare from '../widgets/Mockpare.vue';
 
 import { useAccountStore } from '../../stores/Account';
 import { useAddressStore } from '../../stores/Address';
@@ -356,6 +362,7 @@ export default defineComponent({
         CrossCloseButton,
         Tooltip,
         FeatureToggler,
+        Mockpare,
     },
     directives: {
         responsive: ResponsiveDirective,
@@ -414,6 +421,8 @@ export default defineComponent({
     }
 
     .dev-bar {
+        display: flex;
+        flex-direction: row;
         white-space: nowrap;
     }
 

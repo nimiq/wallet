305588761c00c80d5f275a98a7c07bcb34785a14
Sören <hello@soerenschwert.de>
2022-06-27 14:37:50 +0200

    Link transactions to albatross block explorer

--- src/lib/ExplorerUtils.ts
+++ src/lib/ExplorerUtils.ts
@@ -6,7 +6,7 @@ export function explorerTxLink(asset: CryptoCurrency, hash: string) {
     const { config } = useConfig();
     switch (asset) {
         case CryptoCurrency.NIM:
-            return `https://${config.environment === ENV_MAIN ? '' : 'test.'}nimiq.watch/#${hash}`;
+            return `https://${config.environment === ENV_MAIN ? '' : (config.environment === 'albatross' ? 'albatross.' : 'test.')}nimiq.watch/#${hash}`;
         case CryptoCurrency.BTC:
             return `https://blockstream.info${config.environment === ENV_MAIN ? '' : '/testnet'}/tx/${hash}`;
         case CryptoCurrency.USDC:
@@ -19,11 +19,9 @@ export function explorerAddrLink(asset: SwapAsset, address: string) {
     const { config } = useConfig();
     switch (asset) {
         case SwapAsset.NIM:
-            return `https://${config.environment === ENV_MAIN ? '' : 'test.'}nimiq.watch/`
-                + `#${address}`;
+            return `https://${config.environment === ENV_MAIN ? '' : (config.environment === 'albatross' ? 'albatross.' : 'test.')}nimiq.watch/#${address}`;
         case SwapAsset.BTC:
-            return `https://blockstream.info${config.environment === ENV_MAIN ? '' : '/testnet'}`
-                + `/address/${address}`;
+            return `https://blockstream.info${config.environment === ENV_MAIN ? '' : '/testnet'}/address/${address}`;
         case SwapAsset.USDC:
             return `https://${config.environment === ENV_MAIN ? '' : 'mumbai.'}polygonscan.com/address/${address}`;
         case SwapAsset.EUR:

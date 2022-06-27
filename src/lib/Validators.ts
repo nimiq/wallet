5f01609881b7ef19d763d83b1d9b8fd121eb9bac
Sören <hello@soerenschwert.de>
2022-06-27 14:37:27 +0200

    Add simulated validator registration data, calculate reward rate

---
+++ src/lib/Validators.ts
@@ -0,0 +1,34 @@
+export const validatorData: Record<string, { label: string, fee: number }> = {
+    'NQ70 JM4N PU3R Y2U2 9RGX ABJE VKA4 4TBB HKF1': {
+        label: 'Pooly McPoolface',
+        fee: 0.1 + (Math.random() - 0.5) * 0.1,
+    },
+    'NQ83 P1EH GRM8 HUE8 GS7L CBST UVDS 778L HEX4': {
+        label: 'Cool Pool',
+        fee: 0.1 + (Math.random() - 0.5) * 0.1,
+    },
+    'NQ79 F94K NYV6 3V7D 68X6 FXAD 4PCM XMQR PP4U': {
+        label: 'Puddle',
+        fee: 0.1 + (Math.random() - 0.5) * 0.1,
+    },
+    'NQ94 58R8 YDR5 05EX JTA4 37JU N1EK 6R3Q M9C5': {
+        label: 'Swimming Pool',
+        fee: 0.1 + (Math.random() - 0.5) * 0.1,
+    },
+    'NQ56 BBC5 B9RR PRT9 6V21 TJJ7 PJQ0 N5KA ELAU': {
+        label: 'Kiddie Pool',
+        fee: 0.1 + (Math.random() - 0.5) * 0.1,
+    },
+    'NQ69 GF5T BFJG 9YEX 97VJ JJ27 D9VA 414K 7LG5': {
+        label: 'Pool Billard',
+        fee: 0.1 + (Math.random() - 0.5) * 0.1,
+    },
+    'NQ85 UD3C U8F9 J1L9 J32V X9QX S3NJ BQ4L SCAN': {
+        label: 'Monopooly',
+        fee: 0.1 + (Math.random() - 0.5) * 0.1,
+    },
+    'NQ84 3NMP N7X3 90XQ G8PB 48V3 M15Y B9V0 6C41': {
+        label: 'Not-A-Pool',
+        fee: 0.1 + (Math.random() - 0.5) * 0.1,
+    },
+};

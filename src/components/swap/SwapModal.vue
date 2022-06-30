a0bb4cfa81cea8f9d53d652ea4a76e82982e8bc1
Sören <hello@soerenschwert.de>
2022-06-30 14:16:09 +0200

    Fix address list overflows, address list showing up when not supposed to

--- src/components/swap/SwapModal.vue
+++ src/components/swap/SwapModal.vue
@@ -2424,6 +2424,8 @@ export default defineComponent({
 }
 
 .address-list-overlay {
+    max-height: 100%;
+
     .page-header {
         padding-bottom: 1rem;
     }
@@ -2433,9 +2435,6 @@ export default defineComponent({
         padding: 1rem 0 2rem;
 
         .address-list {
-            height: 100%;
-            overflow-y: auto;
-            overflow-x: hidden;
             padding: 0 2rem;
         }
     }

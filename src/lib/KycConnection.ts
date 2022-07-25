c87ae557d0a6b1e7f676a6dd8cf76b94f874192b
Sören <hello@soerenschwert.de>
2022-07-25 13:15:39 +0200

    Validate stored KYC app grants on app start & account switch

--- src/lib/KycConnection.ts
+++ src/lib/KycConnection.ts
@@ -1,18 +1,40 @@
+import { watch } from '@vue/composition-api';
 import Config from 'config';
 import { useAccountStore } from '../stores/Account';
 import { KycProvider, useKycStore } from '../stores/Kyc';
-import { getAppGrantDetails, init, requestAppGrant } from './TEN31Pass';
+import { getAppGrantDetails, init as initTEN31Pass, requestAppGrant } from './TEN31Pass';
+
+export function init() {
+    initTEN31Pass(Config.TEN31Pass.apiEndpoint, Config.TEN31Pass.appId);
+
+    const { connectedUser } = useKycStore();
+
+    watch(connectedUser, async (user) => {
+        if (!user) return;
+
+        try {
+            const grantDetails = await getAppGrantDetails(user.appGrant);
+            if ('error' in grantDetails) {
+                disconnectKyc();
+            }
+        } catch (error) {
+            console.warn(error); // eslint-disable-line no-console
+        }
+    }, { lazy: false });
+}
 
 export async function connectKyc(provider = KycProvider.TEN31PASS) {
     if (provider !== KycProvider.TEN31PASS) throw new Error('Unsupported KYC provider');
-
-    init(Config.TEN31Pass.apiEndpoint, Config.TEN31Pass.appId);
 
     const accountId = useAccountStore().activeAccountId.value;
     if (!accountId) return;
 
     const appGrant = await requestAppGrant();
     const grantDetails = await getAppGrantDetails(appGrant);
+    if ('error' in grantDetails) {
+        throw new Error('Failed to query app grant: '
+            + `${grantDetails.error}${grantDetails.message ? `: ${grantDetails.message}` : ''}`);
+    }
 
     useKycStore().connect(accountId, {
         provider,
@@ -27,4 +49,6 @@ export function disconnectKyc() {
     if (!accountId) return;
 
     useKycStore().disconnect(accountId);
+
+    // TODO: Show notification that KYC got disconnected
 }

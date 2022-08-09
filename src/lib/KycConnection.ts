869f05f22ce3e6ef168cf457abeddbeb13a35e2e
Daniel <github.danimoh@neverbox.com>
2022-08-09 11:33:03 +0200

    KycConnection: use @nimiq/ten31-pass-api instead of local copy

--- src/lib/KycConnection.ts
+++ src/lib/KycConnection.ts
@@ -1,11 +1,13 @@
 import { watch } from '@vue/composition-api';
+import Ten31PassApi from '@nimiq/ten31-pass-api';
 import Config from 'config';
 import { useAccountStore } from '../stores/Account';
 import { KycProvider, useKycStore } from '../stores/Kyc';
-import { getAppGrantDetails, init as initTEN31Pass, requestAppGrant } from './TEN31Pass';
+
+let ten31PassApi: Ten31PassApi;
 
 export function init() {
-    initTEN31Pass(Config.ten31Pass.apiEndpoint, Config.ten31Pass.appId);
+    ten31PassApi = new Ten31PassApi(Config.ten31Pass.apiEndpoint);
 
     const { connectedUser } = useKycStore();
 
@@ -13,8 +15,8 @@ export function init() {
         if (!user) return;
 
         try {
-            const grantDetails = await getAppGrantDetails(user.appGrant);
-            if ('error' in grantDetails) {
+            if (!await ten31PassApi.getAppGrantInfo(user.appGrant)) {
+                // grant doesn't exist or expired
                 disconnectKyc();
             }
         } catch (error) {
@@ -29,18 +31,16 @@ export async function connectKyc(provider = KycProvider.TEN31PASS) {
     const accountId = useAccountStore().activeAccountId.value;
     if (!accountId) return;
 
-    const appGrant = await requestAppGrant();
-    const grantDetails = await getAppGrantDetails(appGrant);
-    if ('error' in grantDetails) {
-        throw new Error('Failed to query app grant: '
-            + `${grantDetails.error}${grantDetails.message ? `: ${grantDetails.message}` : ''}`);
-    }
+    const grantResponse = await ten31PassApi.requestGrants(Config.ten31Pass.appId);
+    if (!grantResponse) throw new Error('Didn\'t receive a grant response via postMessage');
+    const appGrantDetails = await ten31PassApi.getAppGrantInfo(grantResponse.app);
+    if (!appGrantDetails) throw new Error(`Unknown or expired app grant ${grantResponse.app}`);
 
     useKycStore().connect(accountId, {
         provider,
-        appGrant,
-        id: grantDetails.user.id,
-        name: grantDetails.user.displayName,
+        appGrant: grantResponse.app,
+        id: appGrantDetails.user.id,
+        name: appGrantDetails.user.displayName,
     });
 }
 

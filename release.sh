2f8016b6e172da8ccf6cdc77d9b08fe99522bca4
Daniel <github.danimoh@neverbox.com>
2022-09-12 16:40:27 +0200

    release.sh: makeNginxAllowlist before checking for clean working tree

--- release.sh
+++ release.sh
@@ -18,13 +18,13 @@ yarn utils:getBankList
 # The browserslist utility is meant to run via npx, even when usually using yarn, and is compatible with yarn.lock.
 npx browserslist@latest --update-db
 
+# Build Nginx path allowlist
+yarn utils:makeNginxAllowlist
+
 if [[ `git status --porcelain` ]]; then
     echo "ERROR: The repository has changes. Commit them first, then run again."
     exit 1
 fi
-
-# Build Nginx path allowlist
-yarn utils:makeNginxAllowlist &&
 
 # Tag current commit
 git tag $1 &&

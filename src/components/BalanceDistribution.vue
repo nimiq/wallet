6bd943eb507e5667dc56a404464fd59a9d7ed356
Daniel <github.danimoh@neverbox.com>
2023-02-06 23:24:32 +0100

    BalanceDistribution: add transitions

--- src/components/BalanceDistribution.vue
+++ src/components/BalanceDistribution.vue
@@ -1,18 +1,25 @@
 <template>
-    <!-- eslint-disable vuejs-accessibility/mouse-events-have-key-events -->
+    <!-- eslint-disable vuejs-accessibility/mouse-events-have-key-events, vue/no-parsing-error -->
     <div class="balance-distribution">
-        <svg ref="svg$" xmlns="http://www.w3.org/2000/svg" :viewBox="`0 0 ${svgSize} ${svgSize}`">
+        <transition-group tag="svg" ref="svg$" xmlns="http://www.w3.org/2000/svg"
+            :viewBox="`0 0 ${svgSize} ${svgSize}`"
+            enter-active-class="arc-enter-active"
+            leave-active-class="arc-leave-active"
+            @before-enter="onArcBeforeEnter"
+            @enter="onArcEnter"
+            @leave="onArcLeave">
             <circle v-for="arc in currencyArcs" :key="arc.currency"
                 :r="radius"
                 :stroke-width="STROKE_WIDTH"
                 :stroke-dasharray="`${arc.length} ${arc.spacing}`"
                 :stroke-dashoffset="arc.offset"
                 class="arc" :class="[arc.currency, { 'inactive': arc.inactive }]"
+                :data-currency="arc.currency"
                 @mouseenter="highlightedCurrency = !arc.inactive ? arc.currency : null"
                 @mouseleave="highlightedCurrency = null"
             />
-        </svg>
-        <ul class="breakdown">
+        </transition-group>
+        <transition-group tag="ul" class="breakdown" move-class="breakdown-move">
             <li v-for="record in breakdown" :key="record.currency"
                 :class="{ 'inactive': record.inactive }"
                 @mouseenter="highlightedCurrency = !record.inactive ? record.currency : null"
@@ -20,11 +27,12 @@
                 <div class="currency">{{ record.currency }}</div>
                 <div class="percentage">{{ record.percentage }}</div>
             </li>
-        </ul>
+        </transition-group>
     </div>
 </template>
 
 <script lang="ts">
+import Vue from 'vue';
 import { defineComponent, computed, ref, watch } from '@vue/composition-api';
 import { useAddressStore } from '../stores/Address';
 import { useBtcAddressStore } from '../stores/BtcAddress';
@@ -56,12 +64,17 @@ export default defineComponent({
         const { config } = useConfig();
 
         const STROKE_WIDTH = 4; // px
-        const svg$ = ref<SVGElement>(null);
+        const ARC_GAP = STROKE_WIDTH;
+        const svg$ = ref<Vue>(null);
         const svgSize = ref(0);
         const fullCircleLength = ref(0);
         const highlightedCurrency = ref<SupportedCurrency>(null);
+        const cachedCurrencyArcs = ref<Partial<Record<SupportedCurrency, CurrencyArc>>>(null);
 
         const radius = computed(() => Math.max(0, (svgSize.value - STROKE_WIDTH) / 2));
+        // Rotate by -90 degree to start at the top. Offset by linecap and half gap to have the gap centered at the top.
+        // Note: positive values result in counterclockwise rotation, negative values in clockwise rotation.
+        const circleStart = computed(() => fullCircleLength.value / 4 - STROKE_WIDTH / 2 - ARC_GAP / 2);
 
         const resizeObserver = new ResizeObserver(async ([{ borderBoxSize, contentRect }]) => {
             if (borderBoxSize?.length) {
@@ -72,13 +85,13 @@ export default defineComponent({
             }
             await context.root.$nextTick();
             // Try to measure the actual svg path length if possible because it does not exactly match 2 * PI * r.
-            fullCircleLength.value = svg$.value?.querySelector('circle')?.getTotalLength()
+            fullCircleLength.value = svg$.value?.$el?.querySelector('circle')?.getTotalLength()
                 || 2 * Math.PI * radius.value;
         });
 
         watch(svg$, () => {
-            if (svg$.value) {
-                resizeObserver.observe(svg$.value);
+            if (svg$.value?.$el) {
+                resizeObserver.observe(svg$.value.$el);
             } else {
                 resizeObserver.disconnect();
             }
@@ -108,22 +121,20 @@ export default defineComponent({
         });
 
         const currencyArcs = computed(() => {
-            if (!fullCircleLength.value) return []; // svg measurements not ready yet
+            if (!fullCircleLength.value) return null; // svg measurements not ready yet
 
             const nonEmptyCurrencies = currencies.value.filter((currency) => !!balanceDistribution.value[currency]);
             const arcCurrencies = nonEmptyCurrencies.length ? nonEmptyCurrencies : currencies.value;
             const availableLength = arcCurrencies.length !== 1
-                // There should be a gap of STROKE_WIDTH between each arc and additionally each arc has two line caps of
-                // length STROKE_WIDTH / 2 each which do not count towards the arc's length. Not counting them towards
-                // the length also ensures rendering very small allocations as a dot of at least radius STROKE_WIDTH.
-                ? fullCircleLength.value - arcCurrencies.length * 2 * STROKE_WIDTH
+                // Subtract gaps between each arc and additionally each arc's two line caps of length STROKE_WIDTH / 2
+                // each which do not count towards the arc's length. Not counting them towards the length also ensures
+                // rendering very small allocations as a dot of at least radius STROKE_WIDTH / 2.
+                ? fullCircleLength.value - arcCurrencies.length * (ARC_GAP + STROKE_WIDTH)
                 // If there is just a single currency, render it as full circle without gap and line caps.
                 : fullCircleLength.value;
 
             const result: Partial<Record<SupportedCurrency, CurrencyArc>> = {};
-            // Rotate by -90 degree to start at the top. Offset by linecap (STROKE_WIDTH/2) and half gap to have the gap
-            // centered at the top.
-            let offset = fullCircleLength.value / 4 - STROKE_WIDTH;
+            let offset = circleStart.value;
             for (const currency of arcCurrencies) {
                 const length = nonEmptyCurrencies.length
                     ? balanceDistribution.value[currency] * availableLength
@@ -132,10 +143,12 @@ export default defineComponent({
                 const inactive = !balanceDistribution.value[currency]
                     || (!!highlightedCurrency.value && highlightedCurrency.value !== currency);
                 result[currency] = { currency, length, spacing, offset, inactive };
-                offset -= length + 2 * STROKE_WIDTH; // arc length + line caps + gap
+                offset -= length + STROKE_WIDTH + ARC_GAP; // Advance clockwise by arc length + line caps + gap
             }
             return result;
         });
+
+        watch(currencyArcs, () => cachedCurrencyArcs.value = currencyArcs.value);
 
         const breakdown = computed(() => currencies.value
             .map((currency) => ({
@@ -151,6 +164,61 @@ export default defineComponent({
                 return balanceDistribution.value[currencyA] ? -1 : 1;
             }));
 
+        function onArcBeforeEnter(el: SVGCircleElement) {
+            // Use cachedCurrencyArcs because we want to base the transition start position on where the new arc would
+            // have sit on the old currencyArcs. cachedCurrencyArcs points to the previous arcs at this point because
+            // the watcher updating it runs after this hook.
+            const arcOffset = getArcOffset(cachedCurrencyArcs.value, el.dataset.currency as SupportedCurrency);
+            // Overwrite attribute styles with el.style which has priority.
+            el.style.strokeDasharray = `0 ${fullCircleLength.value}`;
+            if (arcOffset !== null) {
+                el.style.strokeDashoffset = arcOffset.toString();
+            } else {
+                // On initial render transition all arcs in from the circle start.
+                el.style.strokeDashoffset = circleStart.value.toString();
+            }
+            el.style.strokeWidth = '0';
+        }
+
+        async function onArcEnter(el: SVGCircleElement) {
+            await new Promise((resolve) => { window.requestAnimationFrame(resolve); });
+            // Remove style overwrites to start the transition.
+            el.style.strokeDasharray = '';
+            el.style.strokeDashoffset = '';
+            el.style.strokeWidth = '';
+        }
+
+        function onArcLeave(el: SVGCircleElement) {
+            const arcOffset = getArcOffset(currencyArcs.value, el.dataset.currency as SupportedCurrency);
+            if (arcOffset === null) return; // This should only happen at most if the svg gets unmounted.
+            // Overwrite attribute styles with el.style which has priority, to start the transition.
+            el.style.strokeDasharray = `0 ${fullCircleLength.value}`;
+            el.style.strokeDashoffset = arcOffset.toString();
+            el.style.strokeWidth = '0';
+            el.style.opacity = '0.5'; // fade out partially to improve the effect, but use strokeWidth 0 to hide
+        }
+
+        function getArcOffset(
+            arcs: Partial<Record<SupportedCurrency, CurrencyArc>> | null,
+            currency: SupportedCurrency,
+        ) {
+            if (!arcs) return null;
+            if (arcs[currency]) return arcs[currency]!.offset;
+            // The requested arc is not listed in arcs and thus empty. Calculate where it would be sitting if not empty.
+            let predecessorArc: CurrencyArc | null = null;
+            for (const iteratedCurrency of currencies.value) {
+                if (iteratedCurrency === currency) break;
+                if (!arcs[iteratedCurrency]) continue;
+                predecessorArc = arcs[iteratedCurrency]!;
+            }
+            // Place arc in the center of the gap after the predecessor, or in the center of the gap before the first
+            // arc if there is no predecessor. Note: subtracting advances clockwise.
+            const gapCenterOffset = ARC_GAP / 2 + /* line cap */ STROKE_WIDTH / 2;
+            return predecessorArc
+                ? predecessorArc.offset - predecessorArc.length - gapCenterOffset
+                : circleStart.value + gapCenterOffset;
+        }
+
         return {
             STROKE_WIDTH,
             svg$,
@@ -159,6 +227,9 @@ export default defineComponent({
             currencyArcs,
             breakdown,
             highlightedCurrency,
+            onArcBeforeEnter,
+            onArcEnter,
+            onArcLeave,
             testFiatBalances, // for testing the component from the browser's dev tools
         };
     },
@@ -175,27 +246,51 @@ export default defineComponent({
 svg {
     width: 6.375rem;
     height: 6.375rem;
-}
 
-.arc {
-    cx: 50%;
-    cy: 50%;
-    fill: none;
-    stroke-linecap: round;
-    cursor: pointer;
+    .arc {
+        --transition-time: 1.5s;
+        cx: 50%;
+        cy: 50%;
+        fill: none;
+        stroke-linecap: round;
+        cursor: pointer;
+        // regular transition while arc is shown and resizes or changes color due to distribution changes.
+        transition: stroke-dashoffset var(--transition-time) var(--nimiq-ease),
+            stroke-dasharray var(--transition-time) var(--nimiq-ease),
+            stroke calc(.75 * var(--transition-time)) var(--nimiq-ease);
 
-    &.nim {
-        stroke: var(--nimiq-gold);
-    }
-    &.btc {
-        stroke: var(--bitcoin-orange);
-    }
-    &.usdc {
-        stroke: var(--usdc-blue);
-    }
-    &.inactive {
-        stroke: rgba(255, 255, 255, .2);
-        cursor: default;
+        // transition for an arc being added
+        &.arc-enter-active {
+            // Continuously keep the arc via stroke-dashoffset at the correct position without delay, but delay showing
+            // the arc via stroke-dasharray and stroke-width until the other arcs started to move to make some space.
+            transition: stroke-dashoffset var(--transition-time) var(--nimiq-ease),
+                stroke-dasharray calc(.99* var(--transition-time)) calc(.01 * var(--transition-time)) var(--nimiq-ease),
+                stroke-width calc(.1 * var(--transition-time)) calc(.03 * var(--transition-time)) var(--nimiq-ease);
+        }
+
+        // transition for an arc being removed
+        &.arc-leave-active {
+            // Continuously keep the arc via stroke-dashoffset at the correct position without delay, but hide the arc
+            // a quicker via stroke-dasharray, stroke-width and opacity to make some space for the other arcs.
+            transition: stroke-dashoffset var(--transition-time) var(--nimiq-ease),
+                stroke-dasharray calc(.95 * var(--transition-time)) var(--nimiq-ease),
+                stroke-width calc(.1 * var(--transition-time)) calc(.7 * var(--transition-time)) var(--nimiq-ease),
+                opacity calc(.3 * var(--transition-time)) calc(.65 * var(--transition-time)) var(--nimiq-ease);
+        }
+
+        &.nim {
+            stroke: var(--nimiq-gold);
+        }
+        &.btc {
+            stroke: var(--bitcoin-orange);
+        }
+        &.usdc {
+            stroke: var(--usdc-blue);
+        }
+        &.inactive {
+            stroke: rgba(255, 255, 255, .2);
+            cursor: default;
+        }
     }
 }
 
@@ -213,7 +308,7 @@ svg {
         line-height: 1;
         color: white;
         list-style: none;
-        cursor: default;
+        cursor: pointer;
 
         &:not(:first-child) {
             padding-top: .5rem;
@@ -221,10 +316,18 @@ svg {
         &:not(:last-child) {
             padding-bottom: .5rem;
         }
-        &:not(.inactive) {
-            cursor: pointer;
+
+        // Only transitioning position changes on re-sorting, not enter and leave because breakdown records would only
+        // enter or leave when a currency is disabled or enabled in the config.
+        // Opacity transition of the children in combination with the move does not always work, because Vue re-creates
+        // some elements for some reason instead of just moving them, but the transition still looks good enough.
+        &.breakdown-move {
+            transition: transform .5s var(--nimiq-ease);
         }
 
+        * {
+            transition: opacity .5s var(--nimiq-ease);
+        }
         .currency {
             text-transform: uppercase;
         }
@@ -232,8 +335,13 @@ svg {
             margin-left: 1.5rem; // for maintaining a minimum distance to the currency label
             opacity: .5;
         }
-        &.inactive * {
-            opacity: .3;
+
+        &.inactive {
+            cursor: default;
+
+            * {
+                opacity: .3;
+            }
         }
     }
 }

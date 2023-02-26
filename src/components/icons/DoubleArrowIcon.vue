7e923f105fd8b429779084bccac8d2cb4ba6fbad
Mraveux <matheo.raveux@gmail.com>
2023-02-26 22:06:41 +0100

    AccountOverview: add arrow icon in swap buttons’ tooltips

--- src/components/icons/DoubleArrowIcon.vue
+++ src/components/icons/DoubleArrowIcon.vue
@@ -1,5 +1,5 @@
 <template functional>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14" class="double-arrow-icon">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
             d="m1 4 3-3 3 3m0 6-3 3-3-3"/></svg>
 </template>
---
+++ src/components/icons/LinkedDoubleArrowIcon.vue
@@ -0,0 +1,5 @@
+<template functional>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8" class="nq-icon linked-double-arrow-icon">
+        <path stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2"
+            d="M4 7 1 4l3-3m5.5 0 3 3-3 3M1.8 4h9.8" /></svg>
+</template>

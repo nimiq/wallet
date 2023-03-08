b6b1d1ae3d6028705ebc86cee3a456c386550164
Sören <hello@soerenschwert.de>
2023-03-08 15:20:30 +0100

    Set mainnet HTLC contract address

--- src/config/config.mainnet.ts
+++ src/config/config.mainnet.ts
@@ -18,7 +18,7 @@ export default {
         rpcMaxBlockRange: 3_000,
         usdcContract: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
         transferContract: '0x98E69a6927747339d5E543586FC0262112eBe4BD',
-        htlcContract: '',
+        htlcContract: '0xF615bD7EA00C4Cc7F39Faad0895dB5f40891359f',
         relayHubContract: '0x6C28AfC105e65782D9Ea6F2cA68df84C9e7d750d',
         uniswapFactoryContract: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
         uniswapQuoterContract: '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6',

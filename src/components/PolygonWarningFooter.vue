584475f32cb312cb453c10a9a874997f35815bf6
Daniel <github.danimoh@neverbox.com>
2023-03-09 14:51:46 -0600

    UsdcReceiveModal: redesign

--- src/components/PolygonWarningFooter.vue
+++ src/components/PolygonWarningFooter.vue
@@ -1,23 +1,31 @@
 <template>
     <PageFooter class="polygon-warning-footer">
         <div class="nq-text-s" :class="[level, level === 'info' ? 'nq-light-blue' : 'nq-orange']">
-            {{ level === 'info'
-                ? $t('Send to Polygon USDC addresses only!')
-                : $t('Funds sent to non-Polygon USDC addresses will be permanently lost!')
+            {{ type === 'sending'
+                ? level === 'info'
+                    ? $t('Send to Polygon USDC addresses only!')
+                    : $t('Funds sent to non-Polygon USDC addresses could be permanently lost!')
+                : level === 'info'
+                    ? $t('You can receive from Polygon USDC addresses only!')
+                    : $t('Funds sent from non-Polygon USDC addresses could be permanently lost!')
             }}
             <Tooltip :container="this" autoWidth :margin="{
                 left: Math.min(88, .18 * windowWidth),
                 right: Math.min(88, .18 * windowWidth),
             }">
                 <template #trigger><InfoCircleSmallIcon/></template>
-                <p>{{ $t('Ask the receiver to ensure they have a Polygon USDC address.') }}</p>
+                <p>{{ type === 'sending'
+                    ? $t('Ask the receiver to ensure they have a Polygon USDC address.')
+                    : $t('Ask the sender to ensure they have a Polygon USDC address.')
+                }}</p>
                 <p class="explainer">
                     {{ $t('The USDC stablecoin was originally launched on Ethereum. To avoid high fees and '
                     + 'enable fast transactions, this wallet uses the Polygon version.') }}
                 </p>
-                <p class="nq-orange">
-                    {{ $t('Funds sent to non-Polygon USDC versions could be permanently lost.') }}
-                </p>
+                <p class="nq-orange">{{ type === 'sending'
+                    ? $t('Funds sent to non-Polygon USDC versions could be permanently lost.')
+                    : $t('Funds sent from non-Polygon USDC versions could be permanently lost.')
+                }}</p>
             </Tooltip>
         </div>
     </PageFooter>
@@ -30,6 +38,11 @@ import { useWindowSize } from '../composables/useWindowSize';
 
 export default defineComponent({
     props: {
+        type: {
+            type: String,
+            required: true,
+            validator: (type) => ['sending', 'receiving'].includes(type),
+        },
         level: {
             type: String,
             default: 'info',
---
+++ src/components/PolygonWarningPage.vue
@@ -0,0 +1,136 @@
+<template>
+    <div class="polygon-warning-page flex-column">
+        <PageHeader :backArrow="canGoBack" @back="$emit('back')"/>
+        <PageBody class="flex-column">
+            <StopSignIcon/>
+            <h3 class="nq-h3">{{ $t('Before you start') }}</h3>
+            <h1 class="nq-h1">{{ $t('The Nimiq Wallet uses Polygon USDC for speed and low fees.') }}</h1>
+            <p class="nq-text nq-light-blue">
+                <AlertCircleIcon/>
+                {{ type === 'sending'
+                    ? $t('Send to Polygon USDC addresses only!')
+                    : $t('Receive from Polygon USDC addressses only!')
+                }}
+            </p>
+            <p class="nq-text nq-orange">
+                <AlertTriangleIcon/>
+                {{ type === 'sending'
+                    ? $t('Funds sent to non-Polygon USDC addresses could be permanently lost.')
+                    : $t('Funds sent from non-Polygon USDC addresses could be permanently lost.')
+                }}
+            </p>
+            <button class="nq-button light-blue" @click="$emit('continue')" @mousedown.prevent>
+                {{ $t('Got it') }}
+            </button>
+        </PageBody>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import { PageHeader, PageBody, AlertCircleIcon, AlertTriangleIcon } from '@nimiq/vue-components';
+import StopSignIcon from './icons/StopSignIcon.vue';
+
+export default defineComponent({
+    props: {
+        type: {
+            type: String,
+            required: true,
+            validator: (type) => ['sending', 'receiving'].includes(type),
+        },
+        canGoBack: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    components: {
+        PageHeader,
+        PageBody,
+        StopSignIcon,
+        AlertCircleIcon,
+        AlertTriangleIcon,
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.polygon-warning-page {
+    contain: size layout paint style;
+    height: 100%;
+
+    @media (max-width: 700px) { // Full mobile breakpoint
+        contain: size style; // not layout and paint to make page scrollable on mobile
+    }
+}
+
+.page-header {
+    contain: size layout paint style;
+    z-index: 1; // make back arrow accessible over page body
+}
+
+.page-body {
+    contain: size layout paint style;
+    padding-top: 0;
+    margin-top: -3rem;
+    justify-content: space-between;
+    align-items: center;
+
+    @media (max-width: 700px) { // Full mobile breakpoint
+        contain: layout paint style; // not size to make page scrollable on mobile
+    }
+
+    .nq-h3 {
+        margin-top: 4.125rem;
+        margin-bottom: 1.5rem;
+        font-size: var(--small-size);
+        font-weight: 600;
+        line-height: 1;
+        letter-spacing: 0.1875rem;
+        text-transform: uppercase;
+        color: var(--text-60);
+    }
+
+    .nq-h1 {
+        max-width: 41.25rem;
+        margin-top: 0;
+        margin-bottom: 2rem;
+        line-height: 1.3;
+        text-align: center;
+    }
+
+    .nq-text {
+        contain: layout paint style;
+        padding: 2rem;
+        margin: 0;
+        border: 1px solid var(--text-16);
+        align-self: stretch;
+        letter-spacing: .1px;
+
+        &:first-of-type {
+            margin-top: auto;
+            border-bottom: none;
+            border-top-left-radius: 1.5rem;
+            border-top-right-radius: 1.5rem;
+        }
+
+        &:last-of-type {
+            margin-bottom: auto;
+            border-bottom-left-radius: 1.5rem;
+            border-bottom-right-radius: 1.5rem;
+        }
+
+        .nq-icon {
+            display: inline-block;
+            margin-bottom: .5rem;
+            margin-right: .375rem;
+            vertical-align: middle;
+        }
+    }
+
+    .nq-button {
+        contain: size layout paint style;
+        width: calc(100% - 4rem);
+        margin-top: 3rem;
+    }
+}
+</style>

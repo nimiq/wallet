af9a8317747c98bacad60a963c3f3b48d99612fb
Daniel <github.danimoh@neverbox.com>
2023-03-09 14:51:46 -0600

    UsdcReceiveModal: redesign

--- src/components/modals/UsdcReceiveModal.vue
+++ src/components/modals/UsdcReceiveModal.vue
@@ -1,163 +1,234 @@
 <template>
-    <Modal class="receive-modal"
-        :showOverlay="addressQrCodeOverlayOpened || receiveLinkOverlayOpened"
-        @close-overlay="closeOverlays"
-    >
-        <PageHeader :backArrow="!!$route.params.canUserGoBack" @back="back">
+    <Modal class="receive-modal">
+        <transition name="fade">
+            <PolygonWarningPage v-if="page === Pages.WARNING"
+                type="receiving"
+                :canGoBack="!!$route.params.canUserGoBack"
+                @back="back"
+                @continue="page = Pages.RECEIVE"
+            />
+        </transition>
+
+        <PageHeader :backArrow="!!$route.params.canUserGoBack || page !== initialPage" @back="back">
             {{ $t('Receive USDC') }}
-            <div slot="more">{{ $t('Share your address with the sender.') }}</div>
+            <template #more>{{
+                /* Split into multiple strings to share translation of the first sentence with ReceiveModal. */
+                $t('Share your address with the sender.') + ' ' + $t('Make sure they are sending Polygon USDC.')
+            }}</template>
         </PageHeader>
-        <PageBody class="flex-column address-page">
-            <button
-                @click="addressQrCodeOverlayOpened = true"
-                class="reset qr-button"
-            >
-                <QrCodeIcon/>
-            </button>
-            <UsdcIcon />
-            <AddressDisplay :address="address" format="ethereum" :copyable="true"/>
-
-            <div></div> <!-- spacer until another element comes at the bottom -->
-            <!-- <button
-                @click="receiveLinkOverlayOpened = true"
-                @mousedown.prevent
-                class="nq-button-s"
-            >
-                {{ $t('Create request link') }}
-            </button> -->
+        <PageBody class="flex-column">
+            <QrCode
+                :data="address"
+                :size="448"
+                :fill="'#1F2348' /* nimiq-blue */"
+            />
+            <Tooltip ref="addressTooltip$" class="copyable-short-address" noFocus :disabled="isAddressTooltipDisabled">
+                <template #trigger>
+                    <!-- Handle events on Copyable and stop the default events on Tooltip to be able to disable showing
+                    the tooltip on touch, and to handle focus events on the Copyable instead of having the Copyable and
+                    Tooltip both as focus targets during keyboard navigation -->
+                    <Copyable
+                        ref="copyable"
+                        :text="address"
+                        @focus.native.capture.stop="showAddressTooltip"
+                        @blur.native.capture.stop="hideAddressTooltip"
+                        @mouseenter.native.capture.stop="showAddressTooltip"
+                        @mouseleave.native.capture.stop="hideAddressTooltip"
+                        @click.native.capture.stop="$refs.copyable.copy(); /* block tooltip click handler */"
+                        @copy="onCopy">
+                        <ShortAddress :address="address"/>
+                    </Copyable>
+                </template>
+                {{ address }}
+            </Tooltip>
         </PageBody>
-
-        <QrCodeOverlay v-if="addressQrCodeOverlayOpened && address"
-            slot="overlay"
-            qrPrefix="polygon:" :address="address" currency="usdc"
-        />
-
-        <!-- <PaymentLinkOverlay slot="overlay"
-            v-if="receiveLinkOverlayOpened && address"
-            currency="usdc"
-            :address="address"
-        /> -->
+        <PolygonWarningFooter type="receiving"/>
     </Modal>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from '@vue/composition-api';
-import {
-    PageHeader,
-    PageBody,
-    AddressDisplay,
-    QrCodeIcon,
-} from '@nimiq/vue-components';
-import Modal, { disableNextModalTransition } from './Modal.vue';
+import { computed, defineComponent, ref, onMounted, onUnmounted } from '@vue/composition-api';
+import { PageHeader, PageBody, QrCode, Tooltip, Copyable } from '@nimiq/vue-components';
 import { useUsdcAddressStore } from '../../stores/UsdcAddress';
-// import PaymentLinkOverlay from './overlays/PaymentLinkOverlay.vue';
-import QrCodeOverlay from './overlays/QrCodeOverlay.vue';
-import UsdcIcon from '../icons/UsdcIcon.vue';
+import { useUsdcTransactionsStore } from '../../stores/UsdcTransactions';
+import { useWindowSize } from '../../composables/useWindowSize';
+import Modal, { disableNextModalTransition } from './Modal.vue';
+import PolygonWarningPage from '../PolygonWarningPage.vue';
+import ShortAddress from '../ShortAddress.vue';
+import PolygonWarningFooter from '../PolygonWarningFooter.vue';
 
 export default defineComponent({
     name: 'usdc-receive-modal',
     setup(props, context) {
-        const addressQrCodeOverlayOpened = ref(false);
-        const receiveLinkOverlayOpened = ref(false);
-        const { addressInfo } = useUsdcAddressStore();
+        enum Pages {
+            WARNING,
+            RECEIVE,
+        }
+
+        const { state: addresses$, addressInfo } = useUsdcAddressStore();
+        const { state: transactions$ } = useUsdcTransactionsStore();
+        const { isTablet } = useWindowSize();
+        // These are non-reactive because we're only interested in whether the user had ever received USDC when the
+        // modal was initially opened.
+        const normalizedUserAddresses = Object.values(addresses$.addressInfos)
+            .map(({ address }) => address.toLowerCase());
+        const hasEverReceivedUsdc = Object.values(transactions$.transactions)
+            .some(({ recipient }) => normalizedUserAddresses.includes(recipient.toLowerCase()));
+
+        const page = ref(hasEverReceivedUsdc ? Pages.RECEIVE : Pages.WARNING);
+        const initialPage = page.value;
         const address = computed(() => addressInfo.value?.address);
-        function closeOverlays() {
-            addressQrCodeOverlayOpened.value = false;
-            receiveLinkOverlayOpened.value = false;
+        const addressTooltip$ = ref<Tooltip>(null);
+
+        // Disable display of tooltip on touch.
+        const isAddressTooltipDisabled = ref(isTablet.value);
+        function onTouch() {
+            isAddressTooltipDisabled.value = true;
         }
+        // Register touchstart as regular event listener on document, to detect touches on the entire page, and because
+        // event listeners registered via vue as @touchstart are triggered too late for us here to disable the tooltip
+        // in time.
+        onMounted(() => document.addEventListener('touchstart', onTouch));
+        onUnmounted(() => document.removeEventListener('touchstart', onTouch));
+
         function back() {
-            disableNextModalTransition();
-            context.root.$router.back();
+            if (page.value === initialPage) {
+                disableNextModalTransition();
+                context.root.$router.back();
+            } else if (page.value === Pages.RECEIVE) {
+                page.value = Pages.WARNING;
+            }
         }
+
+        let showAddressTooltipTimeout = -1;
+        async function showAddressTooltip(delay: number | Event = 0) {
+            await context.root.$nextTick(); // give isAddressTooltipDisabled some more time to update
+            if (isAddressTooltipDisabled.value || showAddressTooltipTimeout !== -1) {
+                // Tooltip is disabled or showing tooltip is already scheduled, and we want to keep the original delay.
+                return;
+            }
+            delay = typeof delay === 'number' ? delay : 0;
+            showAddressTooltipTimeout = window.setTimeout(() => {
+                showAddressTooltipTimeout = -1;
+                if (!addressTooltip$.value) return;
+                addressTooltip$.value.show();
+            }, delay);
+        }
+        async function hideAddressTooltip() {
+            window.clearTimeout(showAddressTooltipTimeout);
+            showAddressTooltipTimeout = -1;
+            if (!addressTooltip$.value) return;
+            addressTooltip$.value.hide(/* force */ true);
+        }
+
+        function onCopy() {
+            // hide address tooltip and re-show it after the copy tooltip disappeared
+            hideAddressTooltip();
+            isAddressTooltipDisabled.value = false;
+            showAddressTooltip(1500);
+        }
+
         return {
+            Pages,
+            page,
+            initialPage,
             address,
-            addressQrCodeOverlayOpened,
-            receiveLinkOverlayOpened,
-            closeOverlays,
+            addressTooltip$,
+            isAddressTooltipDisabled,
+            showAddressTooltip,
+            hideAddressTooltip,
+            onCopy,
+            isTablet,
             back,
         };
     },
     components: {
         Modal,
+        PolygonWarningPage,
         PageHeader,
         PageBody,
-        AddressDisplay,
-        QrCodeIcon,
-        // PaymentLinkOverlay,
-        QrCodeOverlay,
-        UsdcIcon,
+        QrCode,
+        Tooltip,
+        Copyable,
+        ShortAddress,
+        PolygonWarningFooter,
     },
 });
 </script>
 
 <style lang="scss" scoped>
-.page-body.address-page {
-    justify-content: space-between;
-    align-items: center;
-    padding-bottom: 3rem;
-    position: relative;
+.receive-modal,
+.receive-modal ::v-deep .small-page {
+    contain: size layout paint style; // also clips PolygonWarningPage overflowing small-page's round corners
+}
 
-    .qr-button {
-        position: absolute;
-        right: 3rem;
-        bottom: 3rem;
-        font-size: 4rem;
-        opacity: 0.4;
-        transition: opacity 250ms var(--nimiq-ease);
-
-        svg {
-            display: block;
-        }
-
-        &:hover,
-        &:focus {
-            opacity: 0.8;
-        }
-    }
-
-    .usdc {
-        width: 16rem;
-        margin-top: 3rem;
-        color: var(--usdc-blue);
-    }
-
-    .copyable {
-        padding: 0.5rem;
-        background: var(--nimiq-highlight-bg);
-        border-radius: 0.625rem;
-        margin: 1rem 0 4rem;
-
-        transition:
-            color .3s var(--nimiq-ease),
-            background 0.3s var(--nimiq-ease);
-
-        ::v-deep .background {
-            display: none;
-        }
-
-        .address-display {
-            transition: opacity 0.3s var(--nimiq-ease);
-            font-weight: 500;
-            opacity: 1;
-        }
-
-        &:hover,
-        &:focus,
-        &.copied {
-            background: rgba(5, 130, 202, 0.07); // Based on Nimiq Light Blue
-        }
-    }
+.polygon-warning-page {
+    // style containment set in component itself
+    position: absolute;
+    width: 100%;
+    z-index: 1;
+    background: white;
 }
 
 .page-header {
-    padding-bottom: 2rem;
+    contain: layout paint style;
+    padding-left: 4.5rem;
+    padding-right: 4.5rem;
+    font-size: var(--body-size);
+    line-height: 1.4;
+    font-weight: 600;
+    color: var(--text-60);
 
-    div {
-        font-size: var(--body-size);
-        line-height: 1.4;
-        font-weight: 600;
-        opacity: 0.6;
-        margin-top: 1rem;
+    ::v-deep h1 {
+        margin-bottom: 1rem;
+        color: var(--nimiq-blue);
+    }
+}
+
+.page-body {
+    contain: size layout paint style;
+    padding-top: 2rem;
+    align-items: center;
+
+    .qr-code {
+        // The QrCode is rendered at 2x size and then scaled to half its size,
+        // to be sharp on retina displays:
+        // 2 x 224px = 448px
+        // But now we need to make it behave as half its size as well, that's
+        // why we use negative margins on all sides.
+        --qr-canvas-size: 448px;
+        height: var(--qr-canvas-size); // Must set fixed height for iOS 16 to display correctly
+        margin: calc(var(--qr-canvas-size) / -4);
+        transform: scale(.5);
+    }
+
+    .copyable-short-address {
+        margin-top: 1.25rem;
+
+        .copyable {
+            padding: .25rem .5rem .5rem;
+
+            ::v-deep .tooltip {
+                top: -4.625rem;
+
+                &::after {
+                    transform: scale(-.8) translateY(-.625rem);
+                }
+            }
+        }
+
+        ::v-deep {
+            .trigger::after {
+                transform: scale(-.8) translateY(-.625rem);
+            }
+
+            .tooltip-box {
+                left: 50% !important;
+                padding: .625rem 1rem .5rem;
+                font-size: 1.75rem;
+                transform: translate(-50%, -1.25rem);
+            }
+        }
     }
 }
 </style>
--- src/components/modals/UsdcSendModal.vue
+++ src/components/modals/UsdcSendModal.vue
@@ -7,25 +7,13 @@
         :class="{'value-masked': amountsHidden}"
         ref="modal$"
     >
-        <div v-if="page === Pages.BEFORE_YOU_START" class="page flex-column" :key="Pages.BEFORE_YOU_START">
-            <PageHeader :backArrow="!!$route.params.canUserGoBack" class="header__before-you-start" @back="back"/>
-            <PageBody class="page__before-you-start flex-column">
-                <StopSignIcon/>
-                <h3 class="nq-h3">{{ $t('Before you start') }}</h3>
-                <h1 class="nq-h1">{{ $t('The Nimiq Wallet uses Polygon USDC for speed and low fees.') }}</h1>
-                <p class="nq-text nq-light-blue">
-                    <AlertCircleIcon/>
-                    {{ $t('Send to Polygon USDC addresses only!') }}
-                </p>
-                <p class="nq-text nq-orange">
-                    <AlertTriangleIcon/>
-                    {{ $t('Funds sent to non-Polygon USDC addresses could be permanently lost.') }}
-                </p>
-                <button class="nq-button light-blue" @click="page = Pages.RECIPIENT_INPUT" @mousedown.prevent>
-                    {{ $t('Got it') }}
-                </button>
-            </PageBody>
-        </div>
+        <PolygonWarningPage v-if="page === Pages.WARNING"
+            type="sending"
+            :canGoBack="!!$route.params.canUserGoBack"
+            :key="Pages.WARNING"
+            @back="back"
+            @continue="page = Pages.RECIPIENT_INPUT"
+        />
 
         <div v-if="page === Pages.RECIPIENT_INPUT" class="page flex-column" :key="Pages.RECIPIENT_INPUT">
             <PageHeader :backArrow="!!$route.params.canUserGoBack || page !== initialPage" @back="back">
@@ -67,7 +55,7 @@
                     </span>
                 </section>
             </PageBody>
-            <PolygonWarningFooter level="info"/>
+            <PolygonWarningFooter type="sending" level="info"/>
             <button class="reset scan-qr-button" @click="$router.push('/scan')">
                 <ScanQrCodeIcon/>
             </button>
@@ -95,7 +83,7 @@
                     <template #subline>{{ $t('Long press to confirm') }}</template>
                 </LongPressButton>
             </PageBody>
-            <PolygonWarningFooter level="warning"/>
+            <PolygonWarningFooter type="sending" level="warning"/>
         </div>
 
         <div
@@ -256,8 +244,6 @@ import {
     LongPressButton,
     PageHeader,
     PageBody,
-    AlertCircleIcon,
-    AlertTriangleIcon,
     ScanQrCodeIcon,
     FiatAmount,
     CircleSpinner,
@@ -280,7 +266,7 @@ import { useUsdcAddressStore } from '../../stores/UsdcAddress';
 import { useUsdcContactsStore } from '../../stores/UsdcContacts';
 import { useUsdcNetworkStore } from '../../stores/UsdcNetwork';
 import { useUsdcTransactionsStore } from '../../stores/UsdcTransactions';
-import StopSignIcon from '../icons/StopSignIcon.vue';
+import PolygonWarningPage from '../PolygonWarningPage.vue';
 import PolygonWarningFooter from '../PolygonWarningFooter.vue';
 import AmountInput from '../AmountInput.vue';
 import AmountMenu from '../AmountMenu.vue';
@@ -308,7 +294,7 @@ export default defineComponent({
     },
     setup(props, context) {
         enum Pages {
-            BEFORE_YOU_START,
+            WARNING,
             RECIPIENT_INPUT,
             AMOUNT_INPUT,
         }
@@ -327,7 +313,7 @@ export default defineComponent({
             .map(({ address }) => address.toLowerCase());
         const hasEverSentUsdc = Object.values(transactions$.transactions)
             .some(({ sender }) => normalizedUserAddresses.includes(sender.toLowerCase()));
-        const page = ref(hasEverSentUsdc ? Pages.RECIPIENT_INPUT : Pages.BEFORE_YOU_START);
+        const page = ref(hasEverSentUsdc ? Pages.RECIPIENT_INPUT : Pages.WARNING);
 
         const recipientDetailsOpened = ref(false);
         const recipientWithLabel = ref<{address: string, label: string, type: RecipientType} | null>(null);
@@ -661,7 +647,7 @@ export default defineComponent({
                 disableNextModalTransition();
                 context.root.$router.back();
             } else if (page.value === Pages.RECIPIENT_INPUT) {
-                page.value = Pages.BEFORE_YOU_START;
+                page.value = Pages.WARNING;
             } else if (page.value === Pages.AMOUNT_INPUT) {
                 page.value = Pages.RECIPIENT_INPUT;
                 resetRecipient();
@@ -786,12 +772,10 @@ export default defineComponent({
         Modal,
         PageHeader,
         PageBody,
+        PolygonWarningPage,
         UsdcContactShortcuts,
         UsdcContactBook,
-        StopSignIcon,
         PolygonWarningFooter,
-        AlertCircleIcon,
-        AlertTriangleIcon,
         AddressInput,
         ScanQrCodeIcon,
         LabelInput,
@@ -829,75 +813,15 @@ export default defineComponent({
         position: relative;
     }
 
-    .header__before-you-start ::v-deep .page-header-back-button {
-        z-index: 1; // make back arrow accessible over page body
-    }
-
-    .page__before-you-start {
-        padding-top: 0;
-        margin-top: -3rem;
-
-        .nq-h3 {
-            margin-top: 4.125rem;
-            margin-bottom: 1.5rem;
-            font-size: var(--small-size);
-            font-weight: 600;
-            line-height: 1;
-            letter-spacing: 0.1875rem;
-            text-transform: uppercase;
-            color: var(--text-60);
-        }
-
-        .nq-h1 {
-            max-width: 41.25rem;
-            margin-top: 0;
-            margin-bottom: 2rem;
-            line-height: 1.3;
-            text-align: center;
-        }
-
-        .nq-text {
-            padding: 2rem;
-            margin: 0;
-            border: 1px solid var(--text-16);
-            align-self: stretch;
-            letter-spacing: .1px;
-
-            &:first-of-type {
-                margin-top: auto;
-                border-bottom: none;
-                border-top-left-radius: 1.5rem;
-                border-top-right-radius: 1.5rem;
-            }
-
-            &:last-of-type {
-                margin-bottom: auto;
-                border-bottom-left-radius: 1.5rem;
-                border-bottom-right-radius: 1.5rem;
-            }
-
-            .nq-icon {
-                display: inline-block;
-                margin-bottom: .5rem;
-                margin-right: .375rem;
-                vertical-align: middle;
-            }
-        }
-
-        .nq-button {
-            margin-top: 3rem;
-        }
-    }
-
-    .header__contact-list {
-        padding-bottom: 1.5rem;
-    }
-
     .page__recipient-input,
     .page__contact-list,
     .page__amount-input {
         // 0.375rem to get the distance between the heading and .contact-selection to exact 40px
         padding: 0.375rem 3rem 3rem;
+    }
+
+    .header__contact-list {
+        padding-bottom: 1.5rem;
     }
 
     .page__contact-list {

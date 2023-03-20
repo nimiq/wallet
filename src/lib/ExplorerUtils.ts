e6afbea1135ed449d521cc0f12ae80fe6a501e11
Mraveux <matheo.raveux@gmail.com>
2023-03-20 13:56:30 -0600

    Fixes after rebasing albatross on master
    - fix warnings / lint errors
    - add getHeadHeight & getTransaction methods to albatross rpc client

--- src/lib/ExplorerUtils.ts
+++ src/lib/ExplorerUtils.ts
@@ -6,7 +6,9 @@ export function explorerTxLink(asset: CryptoCurrency, hash: string) {
     const { config } = useConfig();
     switch (asset) {
         case CryptoCurrency.NIM:
-            return `https://${config.environment === ENV_MAIN ? '' : (config.environment === 'albatross' ? 'albatross.' : 'test.')}nimiq.watch/#${hash}`;
+            return `https://${config.environment === ENV_MAIN
+                ? ''
+                : (config.environment === 'albatross' ? 'albatross.' : 'test.')}nimiq.watch/#${hash}`;
         case CryptoCurrency.BTC:
             return `https://blockstream.info${config.environment === ENV_MAIN ? '' : '/testnet'}/tx/${hash}`;
         case CryptoCurrency.USDC:
@@ -20,7 +22,9 @@ export function explorerAddrLink(asset: SwapAsset, address: string) {
     const { config } = useConfig();
     switch (asset) {
         case SwapAsset.NIM:
-            return `https://${config.environment === ENV_MAIN ? '' : (config.environment === 'albatross' ? 'albatross.' : 'test.')}nimiq.watch/#${address}`;
+            return `https://${config.environment === ENV_MAIN
+                ? ''
+                : (config.environment === 'albatross' ? 'albatross.' : 'test.')}nimiq.watch/#${address}`;
         case SwapAsset.BTC:
             return `https://blockstream.info${config.environment === ENV_MAIN ? '' : '/testnet'}/address/${address}`;
         case SwapAsset.USDC:

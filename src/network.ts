f3dc356ebc6b9328bb832becc48257052d20df7f
Sören <hello@soerenschwert.de>
2023-03-25 10:46:20 -0600

    Fix reactivity of network peers

--- src/network.ts
+++ src/network.ts
@@ -44,7 +44,7 @@ export async function launchNetwork() {
 
     const client = await getNetworkClient();
 
-    const { state: network$ } = useNetworkStore();
+    const { state: network$, addPeer, removePeer } = useNetworkStore();
     const transactionsStore = useTransactionsStore();
     const addressStore = useAddressStore();
     const stakingStore = useStakingStore();
@@ -172,14 +172,14 @@ export async function launchNetwork() {
 
     client.addPeerChangedListener((peerId, reason, peerCount, peerInfo) => {
         if (reason === 'joined' && peerInfo) {
-            network$.peers[peerId] = {
+            addPeer({
                 peerId,
                 multiAddress: peerInfo.address,
                 nodeType: peerInfo.type,
                 connected: true,
-            };
+            });
         } else if (reason === 'left') {
-            delete network$.peers[peerId];
+            removePeer(peerId);
         }
         network$.peerCount = peerCount;
     });

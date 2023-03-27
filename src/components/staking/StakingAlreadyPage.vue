2101eb3113d07825f2b28f2de6f24c0638647d74
Mraveux <matheo.raveux@gmail.com>
2023-03-27 10:23:35 -0600

    StakingModals: remove the graph placeholder text

--- src/components/staking/StakingAlreadyPage.vue
+++ src/components/staking/StakingAlreadyPage.vue
@@ -33,9 +33,9 @@
                 </Tooltip>
             </span>
             <div style="height: 150px; background: gainsboro;">
-                <h2
+                <!-- <h2
                     style="font-size: 32px; font-weigth: bold; color: darkgrey; text-align: center; margin-top: 56px;"
-                >📈 Graph will come here</h2>
+                >📈 Graph will come here</h2> -->
             </div>
             <!-- <StakingGraph :stakedAmount="stake ? stake.balance : 0"
                 :apy="validator && 'reward' in validator ? validator.reward : 0" :readonly="true"

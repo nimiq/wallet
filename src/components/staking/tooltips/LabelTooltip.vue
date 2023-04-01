277903a600f4b4f84b46e33e8e3b4690c4459cd1
Mraveux <matheo.raveux@gmail.com>
2023-04-01 17:55:47 -0600

    Extract and reuse the validator description tooltip

--- src/components/staking/tooltips/LabelTooltip.vue
+++ src/components/staking/tooltips/LabelTooltip.vue
@@ -1,61 +1,39 @@
 <template>
-    <Tooltip class="validator-label-tip"
-        preferredPosition="bottom right"
-        :styles="{width: 'description' in validator ? '40rem' : '55.25rem', 'text-align': 'left'}"
-    >
-        <div slot="trigger" class="validator-label-trigger flex-row">
-            <img v-if="'icon' in validator" :src="`/img/staking/providers/${validator.icon}`" :alt="validator.label" />
+    <ValidatorDescriptionTooltip :validator="validator" preferredPosition="bottom">
+        <div class="validator-label-trigger flex-row">
+            <img v-if="'icon' in validator" :src="`/img/staking/providers/${validator.icon}`"
+                :alt="validator.label" />
             <Identicon v-else :address="validator.address"/>
 
             <span v-if="'label' in validator" class="validator-label">{{ validator.label }}</span>
             <ShortAddress v-else :address="validator.address"/>
         </div>
-
-        <template v-if="'description' in validator">
-            <p class="validator-description">
-                {{ validator.description }}
-            </p>
-            <p class="validator-disclaimer">
-                {{ $t('The validator is solely responsible for information provided above.') }}
-            </p>
-            <br />
-            <BlueLink v-if="validator.link" :href="validator.link" target="_blank" rel="noopener">
-                {{ $t('{poolName} Website', { poolName: validator.label }) }}
-            </BlueLink>
-        </template>
-        <p v-else>{{ validator.address }}</p>
-    </Tooltip>
+    </ValidatorDescriptionTooltip>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
-import { Tooltip, Identicon } from '@nimiq/vue-components';
-import BlueLink from '../../BlueLink.vue';
+import { Identicon } from '@nimiq/vue-components';
 import { Validator } from '../../../stores/Staking';
-import ShortAddress from '../../ShortAddress.vue';
+import ValidatorDescriptionTooltip from './ValidatorDescriptionTooltip.vue';
 
 export default defineComponent({
     props: {
+        // container: HTMLElement,
         validator: {
             type: Object as () => Validator,
             required: true,
         },
     },
     components: {
-        Tooltip,
         Identicon,
-        BlueLink,
-        ShortAddress,
+        ValidatorDescriptionTooltip,
     },
 });
 </script>
 
 <style lang="scss" scoped>
 @import '../../../scss/functions.scss';
-
-.validator-label-tip {
-    z-index: 2;
-}
 
 .validator-label-trigger {
     align-items: center;
@@ -82,30 +60,5 @@ export default defineComponent({
     .short-address {
         font-weight: 500;
     }
-}
-.validator-description {
-    margin-left: .75rem;
-    padding-left: 1.25rem;
-
-    font-weight: 600;
-    font-size: 2rem;
-    line-height: 130%;
-
-    color: white;
-    border-left: .1875rem solid rgba(255, 255, 255, 0.4);
-}
-
-.validator-disclaimer {
-    margin-bottom: -1rem;
-    font-weight: 600;
-    font-size: 1.75rem;
-    line-height: 130%;
-
-    opacity: 0.6;
-    width: 31.5rem;
-}
-
-.blue-link {
-    color: var(--nimiq-light-blue-on-dark);
 }
 </style>
---
+++ src/components/staking/tooltips/ValidatorDescriptionTooltip.vue
@@ -0,0 +1,88 @@
+<template>
+    <Tooltip class="validator-description-tooltip" :preferredPosition="preferredPosition"
+        :container="container && { $el: container }" @click.native.stop>
+        <slot slot="trigger" />
+
+        <blockquote>
+            <p v-if="'description' in validator" class="description">{{ validator.description }}</p>
+            <span v-else class="no-description">{{ $t('No description available') }}</span>
+        </blockquote>
+
+        <p>{{ $t('The validator is solely responsible for information provided above.') }}</p>
+        <BlueLink v-if="'link' in validator && 'label' in validator"
+            :href="validator.link" target="_blank" rel="noopener">
+            {{ $t('{poolName} Website', { poolName: validator.label }) }}
+        </BlueLink>
+
+    </Tooltip>
+</template>
+
+<script lang="ts">
+import { Tooltip } from '@nimiq/vue-components';
+import { defineComponent } from '@vue/composition-api';
+import { Validator } from '../../../stores/Staking';
+import BlueLink from '../../BlueLink.vue';
+
+export default defineComponent({
+    props: {
+        container: HTMLElement,
+        validator: {
+            type: Object as () => Validator,
+            required: true,
+        },
+        preferredPosition: {
+            type: String,
+            default: 'top',
+        },
+    },
+    components: {
+        Tooltip,
+        BlueLink,
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.tooltip {
+    margin-left: 0.625rem;
+
+    ::v-deep .tooltip-box {
+        width: 31.5rem;
+        line-height: 130%;
+        cursor: default;
+        text-align: left;
+        z-index: 2;
+    }
+
+    &[class*='position-top'] ::v-deep .tooltip-box { transform: translateY(-1.9rem) }
+    &[class*='position-bottom'] ::v-deep .tooltip-box { transform: translateY(1.9rem) }
+}
+
+blockquote {
+    margin: 0;
+    padding-left: 1.25rem;
+    border-left: 1.5px solid rgba(white, .4);
+
+    font-size: 2rem;
+    line-height: 130%;
+
+    p.description {
+        color: white;
+        margin: 0;
+    }
+    span.no-description {
+        opacity: .25;
+        font-style: italic;
+    }
+}
+
+p:not(.description) {
+    opacity: .6;
+    font-size: 1.75rem;
+    line-height: 130%;
+}
+
+.blue-link {
+    color: var(--nimiq-light-blue-on-dark);
+}
+</style>

e3af1a42ef0f924f4139c8d33177aec216209a1b
Mraveux <matheo.raveux@gmail.com>
2023-04-01 21:55:31 -0600

    StakingModal: make the validator list an overlay
    & add Overlay enum
    & simplify the Page enum
    & rename StakingValidatorPage.vue to StakingValidatorOverlay.vue

--- src/components/staking/StakingModal.vue
+++ src/components/staking/StakingModal.vue
@@ -1,31 +1,39 @@
 <template>
     <!-- Pass down all attributes not declared as props -->
     <Modal v-bind="$attrs" v-on="$listeners" class="staking-modal"
-        :class="{ 'fat-modal': page >= Page.GraphPage }"
-        :showOverlay="page === Page.ValidatorPage && invalidAccount"
-        @close-overlay="page = Page.InfoPage"
+        :class="{ 'fat-modal': page >= Page.Graph }"
+        :showOverlay="overlay === Overlay.SelectAccount || overlay === Overlay.Validator"
+        @close-overlay="closeOverlay"
     >
-        <template v-if="page === Page.InfoPage">
-            <StakingInfoPage @next="page = Page.ValidatorPage" />
+        <template v-if="page === Page.Info">
+            <StakingInfoPage @next="switchValidator" />
         </template>
-        <template v-if="page === Page.ValidatorPage">
-            <StakingValidatorPage @back="page = isStaking ? Page.AlreadyPage : Page.InfoPage"
-                @next="page = isStaking ? Page.AlreadyPage : Page.GraphPage"/>
+        <template v-if="page === Page.Graph">
+            <StakingGraphPage
+                @back="isStaking ? (page = Page.Already) : (page = Page.Info)"
+                @next="page = Page.Already"
+                @changeValidator="switchValidator"
+            />
         </template>
-        <template v-if="page === Page.GraphPage">
-            <StakingGraphPage @back="page = isStaking ? Page.ValidatorPage : Page.InfoPage"
-                @next="page = Page.AlreadyPage" />
-        </template>
-        <template v-if="page === Page.AlreadyPage">
-            <StakingAlreadyPage @back="page = Page.GraphPage" @next="page = Page.RewardsHistoryPage"
+        <template v-if="page === Page.Already">
+            <StakingAlreadyPage
+                @back="page = Page.Graph" @next="page = Page.RewardsHistory"
                 @adjust-stake="adjustStake"
-                @switch-validator="switchValidator" />
+                @switch-validator="switchValidator"
+            />
         </template>
-        <template v-if="page === Page.RewardsHistoryPage">
-            <StakingRewardsHistoryPage @back="page = Page.AlreadyPage" />
+        <template v-if="page === Page.RewardsHistory">
+            <StakingRewardsHistoryPage @back="page = Page.Already" />
         </template>
-
-        <SelectAccountOverlay slot="overlay" />
+        <template slot="overlay">
+            <SelectAccountOverlay v-if="overlay === Overlay.SelectAccount"
+                @selected="switchValidator"
+            />
+            <StakingValidatorPage v-if="overlay === Overlay.Validator"
+                @back="page = isStaking ? Page.Already : Page.Info"
+                @next="page = Page.Graph; closeOverlay()"
+            />
+        </template>
     </Modal>
 </template>
 
@@ -35,47 +43,60 @@ import { useStakingStore } from '../../stores/Staking';
 import { useAddressStore } from '../../stores/Address';
 import Modal from '../modals/Modal.vue';
 import StakingInfoPage from './StakingInfoPage.vue';
-import StakingValidatorPage from './StakingValidatorPage.vue';
+import StakingValidatorPage from './StakingValidatorOverlay.vue';
 import StakingGraphPage from './StakingGraphPage.vue';
 import StakingAlreadyPage from './StakingAlreadyPage.vue';
 import StakingRewardsHistoryPage from './StakingRewardsHistoryPage.vue';
 import SelectAccountOverlay from './SelectAccountOverlay.vue';
 
 enum Page {
-    InfoPage,
-    ValidatorPage,
-    GraphPage,
-    AlreadyPage,
-    RewardsHistoryPage,
+    Info,
+    Graph,
+    Already,
+    RewardsHistory,
+}
+
+enum Overlay {
+    SelectAccount,
+    Validator,
 }
 
 export default defineComponent({
     setup() {
         const { activeAddressInfo } = useAddressStore();
         const { activeValidator, activeStake } = useStakingStore();
-        const page = ref(activeValidator.value ? Page.AlreadyPage : Page.InfoPage);
+        const page = ref(activeValidator.value ? Page.Already : Page.Info);
+        const overlay = ref<Overlay | null>(null);
 
         const invalidAccount = computed(() => activeAddressInfo.value
             ? !activeAddressInfo.value.balance && !activeStake.value?.balance
             : false);
 
-        const adjustStake = () => { page.value = Page.GraphPage; };
-        const switchValidator = () => { page.value = Page.ValidatorPage; };
+        const adjustStake = () => { page.value = Page.Graph; };
+        const switchValidator = () => { overlay.value = Overlay.Validator; };
+        const closeOverlay = () => { overlay.value = null; };
 
-        const isStaking = computed(() => activeStake.value && activeStake.value.balance);
+        const isStaking = computed(() => !!(activeStake.value || activeValidator.value));
 
         watch(activeStake, (stake) => {
-            if (!stake) page.value = Page.InfoPage;
+            if (!stake) page.value = Page.Info;
         }, { lazy: true });
+
+        watch(overlay, (newOverlay) => {
+            if (newOverlay === Overlay.Validator && invalidAccount.value) overlay.value = Overlay.SelectAccount;
+        });
 
         return {
             page,
+            overlay,
             activeValidator,
             adjustStake,
             switchValidator,
+            closeOverlay,
             invalidAccount,
             isStaking,
             Page,
+            Overlay,
         };
     },
     components: {
@@ -94,6 +115,8 @@ export default defineComponent({
     .modal {
         &.fat-modal {
             ::v-deep .small-page {
+                transition: transform var(--transition-time) var(--nimiq-ease),
+                            width 200ms var(--nimiq-ease);
                 width: 63.5rem;
                 // height: 74.875rem;
                 // transition: width 1.5s ease-out;

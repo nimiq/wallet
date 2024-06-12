bf6af2b7513f821eed69c2a04bea60223ee9a5df
Mraveux <matheo.raveux@gmail.com>
2024-06-12 10:29:28 +0300

    PreStaking: add tooltip to PrestakingNotice

--- src/components/prestaking/PrestakingNotice.vue
+++ src/components/prestaking/PrestakingNotice.vue
@@ -1,11 +1,23 @@
 <template>
     <div :class="`prestaking-notice ${theme === 'warning' ? 'nq-orange' : ''}`">
         {{ $t('Prestaked funds are locked until launch') }}
+        <Tooltip preferredPosition="top">
+            <InfoCircleSmallIcon slot="trigger"/>
+            <template>
+                <p>{{ $t('Prestaked funds remain locked until the Proof of Stake network goes live.') }}</p>
+                <BlueLink href="https://www.nimiq.com/blog/migration-process-to-nimiq-proof-of-stake/">
+                    <!-- TODO: Change to the correct link -->
+                    {{ $t('Learn more') }}
+                </BlueLink>
+            </template>
+        </Tooltip>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from '@vue/composition-api';
+import { Tooltip, InfoCircleSmallIcon } from '@nimiq/vue-components';
+import BlueLink from '../BlueLink.vue';
 
 export default defineComponent({
     props: {
@@ -14,11 +26,16 @@ export default defineComponent({
             default: 'normal',
         },
     },
+    components: {
+        Tooltip,
+        InfoCircleSmallIcon,
+        BlueLink,
+    },
 });
 </script>
 
 <style lang="scss" scoped>
-    @import '../../scss/mixins.scss';
+@import '../../scss/mixins.scss';
 
 .prestaking-notice {
     font-size: var(--small-size);
@@ -26,5 +43,22 @@ export default defineComponent({
     margin-top: 3rem;
     line-height: 1;
     color: var(--text-50);
+}
+
+.tooltip {
+    margin-left: 0.5rem;
+}
+
+.tooltip ::v-deep .tooltip-box {
+    width: 28rem;
+}
+
+p {
+    margin-bottom: 1rem;
+    text-align: left;
+}
+
+.blue-link {
+    color: #0CA6FE; // $nimiq-blue on dark
 }
 </style>

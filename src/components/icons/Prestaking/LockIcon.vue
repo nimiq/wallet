9c0c98084ca8a8049e8785ae21d8bdfd734a1476
Mraveux <matheo.raveux@gmail.com>
2024-06-12 11:44:37 +0300

    PreStaking: add PrestakingPreview and inmplement
    + LockIcon.vue

---
+++ src/components/icons/Prestaking/LockIcon.vue
@@ -0,0 +1,11 @@
+<template functional>
+    <svg class="nq-icon lock-icon" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <mask id="hole">
+                <rect width="22" height="22" fill="white"/>
+                <path fill="none" stroke="black" stroke-width="1.5" d="M14.2 9.5H7.8c-.442 0-.8.298-.8.667v4.666c0 .368.358.667.8.667h6.4c.442 0 .8-.299.8-.667v-4.666c0-.369-.358-.667-.8-.667ZM8.75 9.25v-1.5a2.25 2.25 0 1 1 4.5 0v1.5"/>
+            </mask>
+        </defs>
+        <rect width="22" height="22" rx="11" fill="#fff" mask="url(#hole)" />
+    </svg>
+</template>

d3bdca043723aa2b7a69679ebb76c643cf17df5a
Sören <hello@soerenschwert.de>
2024-09-23 14:34:23 +0200

    Re-enable BTC swapping

--- src/config/config.mainnet.ts
+++ src/config/config.mainnet.ts
@@ -40,7 +40,7 @@ export default {
 
     fastspot: {
         enabled: true, // Set to false to hide the swap feature.
-        enabledSwapAssets: [SwapAsset.NIM, SwapAsset.USDC_MATIC],
+        enabledSwapAssets: [SwapAsset.NIM, SwapAsset.BTC, SwapAsset.USDC_MATIC],
         apiEndpoint: 'https://api.go.fastspot.io/fast/v1',
         // This is a publishable key
         apiKey: 'c20d43d0-8f60-4fca-a298-85e80f64d042',

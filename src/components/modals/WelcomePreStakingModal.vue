74777c0d5bc49cf32bc5a0b3169591b6a29cdb14
Mraveux <matheo.raveux@gmail.com>
2024-09-30 13:14:41 +0200

    Welcome Prestaking: close behavior
    - hide close icon
    - implement modal close on button click
    - prevent close on swipe

--- src/components/modals/WelcomePreStakingModal.vue
+++ src/components/modals/WelcomePreStakingModal.vue
@@ -1,5 +1,8 @@
 <template>
-    <Modal v-if="!welcomePreStakingModalAlreadyShown" class="welcome-prestaking-modal" :emitClose="true">
+    <Modal class="welcome-prestaking-modal" ref="modal$" v-bind="$attrs" v-on="$listeners"
+        v-if="!welcomePreStakingModalAlreadyShown"
+        emitClose :swipeToClose="false"
+    >
         <PageBody class="flex-column">
             <h1 class="nq-h1">{{ $t('Welcome to Pre-Staking!') }}</h1>
             <p class="nq-text">
@@ -13,29 +16,28 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, ref } from '@vue/composition-api';
 import { PageBody } from '@nimiq/vue-components';
-import { useConfig } from '../../composables/useConfig';
 import { WELCOME_PRE_STAKING_MODAL_LOCALSTORAGE_KEY } from '../../lib/Constants';
 import Modal from './Modal.vue';
 
 export default defineComponent({
     setup() {
-        const { config } = useConfig();
-        // Split the long line into two lines
+        const modal$ = ref<Modal>(null);
+
         const welcomePreStakingModalAlreadyShown = !!window.localStorage.getItem(
             WELCOME_PRE_STAKING_MODAL_LOCALSTORAGE_KEY,
         );
 
-        function closeModal() {
+        async function closeModal() {
             window.localStorage.setItem(WELCOME_PRE_STAKING_MODAL_LOCALSTORAGE_KEY, 'true');
-            // Close the modal or navigate away
-            // You might want to use router.push or emit an event to close the modal
+            await modal$.value!.forceClose();
         }
 
         return {
             welcomePreStakingModalAlreadyShown,
             closeModal,
+            modal$,
         };
     },
     components: {
@@ -46,6 +48,10 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+.modal ::v-deep .close-button {
+    display: none;
+}
+
 .welcome-prestaking-modal {
     .nq-h1 {
         margin-bottom: 2rem;

7c2ed78d9b3f0fce6d4989d3eea4d51cd4af8d59
Mraveux <matheo.raveux@gmail.com>
2024-10-05 16:33:53 +0200

    Prestaking: change background location

--- src/components/modals/WelcomePreStakingModal.vue
+++ src/components/modals/WelcomePreStakingModal.vue
@@ -179,7 +179,7 @@ export default defineComponent({
 
 .right-column {
     width: 100%; // Full width on mobile
-    background: url('../../../public/img/prestaking/welcome/background.png') no-repeat center center;
+    background: url('../../../public/img/prestaking/background.png') no-repeat center center;
     background-size: cover;
     background-color: nimiq-blue(1);
     color: white;

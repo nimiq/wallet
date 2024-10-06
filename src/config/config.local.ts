646b3f7b10a40457659caf3ccb10a0453d4e53fa
Sören <hello@soerenschwert.de>
2024-10-06 09:30:12 +0200

    Re-introduce block-based pre-staking window for transaction detection
    For the validity of pre-staking transactions, the on-chain window is the defining one.

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -13,6 +13,13 @@ export default {
     pageVisibilityTxRefreshInterval: 1 * 60e3, // 1 minute
 
     prestaking: {
+        // The block heights determining the on-chain pre-staking window. All transactions inside this window count
+        // for pre-staking.
+        startBlock: 3_392_200, // 2024-10-06T02:53:18Z
+        endBlock: 3_456_000, // ~2024-11-19T07:00:00Z
+        // The dates determining the display of the pre-staking UI in the Wallet. This window is deliberately smaller
+        // than the on-chain window to allow for grace periods for the UI to be updated.
+        // The UI is still shown for address who have pre-staked even after the `endDate`.
         startDate: new Date('2024-10-07T00:00:00Z'),
         endDate: new Date('2024-11-11T00:00:00Z'),
         totalPrestakeEndpoint: 'https://v2.test.nimiqwatch.com/api/v2/total-prestake',

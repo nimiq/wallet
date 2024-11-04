55cdd4cb2d13d44868a20c986a6257dcadacf696
Sören <hello@soerenschwert.de>
2024-11-04 10:02:24 +0100

    Add Garuda Pool

--- src/stores/Prestaking.ts
+++ src/stores/Prestaking.ts
@@ -161,6 +161,10 @@ export const usePrestakingStore = createStore({
             },
             'NQ15 HNAH YRVH DFVM YHAG BSXG 0QHK KA0Q XDR7': {
                 address: 'NQ15 HNAH YRVH DFVM YHAG BSXG 0QHK KA0Q XDR7',
+                label: 'Garuda Pool',
+                // description: '',
+                icon: 'garudapool.png',
+                // website: '',
                 stake: null,
             },
             'NQ19 AC1J 0S6V KDYU 6YDQ RC57 9VH9 EEHP 9RFK': {

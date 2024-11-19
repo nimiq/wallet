f93e6d3721098e595083baa1954a124b9d769e51
Mraveux <matheo.raveux@gmail.com>
2024-11-19 01:37:16 -0600

    StakingModal: smol refactor + validator overlay

--- src/components/staking/StakingModal.vue
+++ src/components/staking/StakingModal.vue
@@ -1,23 +1,22 @@
 <template>
-    <!-- Pass down all attributes not declared as props -->
     <Modal v-bind="$attrs" v-on="$listeners" class="staking-modal"
-        :class="{ 'large-modal': [Page.Graph, Page.Already].includes(page) }"
-        :showOverlay="overlay === Overlay.SelectAccount || statusType !== StatusChangeType.NONE"
+        :class="{ 'large-modal': [Page.Graph, Page.Info].includes(page) }"
+        :showOverlay="shouldShowOverlay"
         @close-overlay="closeOverlay"
     >
         <transition name="fade" mode="out-in">
-            <template v-if="page === Page.Info">
+            <template v-if="page === Page.Welcome">
                 <StakingWelcomePage @next="switchValidator" />
             </template>
             <template v-else-if="page === Page.Graph">
                 <StakingGraphPage
-                    @back="page = isStaking ? Page.Already : Page.Validator"
-                    @next="page = Page.Already"
-                    @changeValidator="switchValidator"
+                    @back="page = isStaking ? Page.Info : Page.ValidatorList"
+                    @next="page = Page.Info"
+                    @selectValidator="onSelectValidator"
                     @statusChange="onStatusChange"
                 />
             </template>
-            <template v-else-if="page === Page.Already">
+            <template v-else-if="page === Page.Info">
                 <StakingInfoPage
                     @back="page = Page.Graph" @next="page = Page.RewardsHistory"
                     @adjust-stake="adjustStake"
@@ -26,14 +25,15 @@
                 />
             </template>
             <template v-else-if="page === Page.RewardsHistory">
-                <StakingRewardsHistoryPage @back="page = Page.Already" />
+                <StakingRewardsHistoryPage @back="page = Page.Info" />
             </template>
-            <template v-else-if="page === Page.Validator">
+            <template v-else-if="page === Page.ValidatorList">
                 <StakingValidatorPage
-                    @back="page = isStaking ? Page.Already : Page.Info"
-                    @next="page = isStaking ? Page.Already : Page.Graph; closeOverlay()"
+                    @back="page = isStaking ? Page.Info : Page.Welcome"
+                    @selectValidator="onSelectValidator"
                     @statusChange="onStatusChange"
                 />
+                    <!-- @next="page = isStaking ? Page.Info : Page.Graph; closeOverlay()" -->
             </template>
         </transition>
         <template slot="overlay">
@@ -45,15 +45,19 @@
                 :title="statusTitle"
                 :message="statusMessage"
             />
-                <!-- :mainAction="$t('Cancel')"
-                @main-action="onStatusMainAction" -->
+            <ValidatorDetailsOverlay v-else-if="overlay === Overlay.ValidatorDetails"
+                :noButton="page !== Page.ValidatorList"
+                :validator="selectedValidator"
+                @statusChange="onStatusChange"
+                @next="onConfirmValidator"
+            />
         </template>
     </Modal>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, computed, watch, onBeforeUnmount } from '@vue/composition-api';
-import { useStakingStore } from '../../stores/Staking';
+import { useStakingStore, Validator } from '../../stores/Staking';
 import { useAddressStore } from '../../stores/Address';
 import Modal from '../modals/Modal.vue';
 import StakingWelcomePage from './StakingWelcomePage.vue';
@@ -63,18 +67,19 @@ import StakingInfoPage from './StakingInfoPage.vue';
 import StakingRewardsHistoryPage from './StakingRewardsHistoryPage.vue';
 import SelectAccountOverlay from './SelectAccountOverlay.vue';
 import StatusScreen, { State } from '../StatusScreen.vue';
+import ValidatorDetailsOverlay from './ValidatorDetailsOverlay.vue';
 
 enum Page {
-    Info,
-    Graph,
-    Already,
-    RewardsHistory,
-    Validator,
+    Welcome, // StakingWelcomePage
+    Graph, // StakingGraphPage
+    Info, // StakingInfoPage
+    RewardsHistory, // StakingRewardsHistoryPage
+    ValidatorList, // StakingValidatorPage
 }
 
 enum Overlay {
     SelectAccount,
-    Validator,
+    ValidatorDetails,
 }
 
 export enum StatusChangeType {
@@ -85,11 +90,19 @@ export enum StatusChangeType {
     DEACTIVATING,
 }
 
+type StatusChangeParams = {
+    type?: StatusChangeType,
+    state?: State,
+    title?: string,
+    message?: string,
+    timeout?: number,
+};
+
 export default defineComponent({
     setup() {
         const { activeAddressInfo } = useAddressStore();
         const { activeValidator, activeStake } = useStakingStore();
-        const page = ref(activeValidator.value ? Page.Already : Page.Info);
+        const page = ref(activeValidator.value ? Page.Info : Page.Welcome);
         const overlay = ref<Overlay | null>(null);
 
         const isStaking = computed(() => !!(activeStake.value?.activeBalance || activeStake.value?.inactiveBalance));
@@ -98,12 +111,10 @@ export default defineComponent({
             : false);
 
         const adjustStake = () => { page.value = Page.Graph; };
-        const switchValidator = () => { page.value = Page.Validator; };
+        const switchValidator = () => { page.value = Page.ValidatorList; };
         const closeOverlay = () => { overlay.value = null; };
 
-        /**
-         * Status Screen
-         */
+        /* Status Screen */
         const statusType = ref<StatusChangeType>(StatusChangeType.NONE);
         const statusState = ref<State>(State.LOADING);
         const statusTitle = ref('');
@@ -112,16 +123,6 @@ export default defineComponent({
         const statusMainAction = ref('');
         const statusAlternativeAction = ref('');
 
-        type StatusChangeParams = {
-            type?: StatusChangeType,
-            state?: State,
-            title?: string,
-            message?: string,
-            timeout?: number,
-        };
-
-        let statusChangeTimeout: number | null = null;
-
         function updateStatusScreen({ type, state, title, message }: StatusChangeParams) {
             if (type !== undefined) statusType.value = type;
             if (state !== undefined) statusState.value = state;
@@ -129,6 +130,7 @@ export default defineComponent({
             if (message !== undefined) statusMessage.value = message;
         }
 
+        let statusChangeTimeout: number | null = null;
         async function onStatusChange(statusChangeObj: StatusChangeParams) {
             if (statusChangeTimeout) clearTimeout(statusChangeTimeout);
 
@@ -141,23 +143,41 @@ export default defineComponent({
             }
         }
 
-        // function onStatusMainAction() {
-        //     // Handle main action here
-        // }
-
-        watch(activeStake, (stake) => {
-            if (!stake) page.value = Page.Info;
-        }, { lazy: true });
-
+        /* Overlays */
         function showOverlayIfInvalidAccount(newPath: Overlay | Page) {
-            if (newPath === Page.Validator && invalidAccount.value) {
+            if (newPath === Page.ValidatorList && invalidAccount.value) {
                 overlay.value = Overlay.SelectAccount;
             }
         }
 
+        const shouldShowOverlay = computed(() =>
+            overlay.value === Overlay.SelectAccount
+            || statusType.value !== StatusChangeType.NONE
+            || overlay.value === Overlay.ValidatorDetails,
+        );
+
+        /* Event Handlers */
+        const selectedValidator = ref<Validator | null>(null);
+        function onSelectValidator(validator: Validator) {
+            selectedValidator.value = validator;
+            overlay.value = Overlay.ValidatorDetails;
+        }
+
+        function onConfirmValidator() {
+            selectedValidator.value = null;
+            overlay.value = null;
+            page.value = Page.Graph;
+        }
+
+        /* Watchers */
+        watch(activeStake, (stake) => {
+            if (!stake) page.value = Page.Welcome;
+        }, { lazy: true });
+
         watch(page, (newPage) => showOverlayIfInvalidAccount(newPage as Page));
         watch(overlay, (newOverlay) => showOverlayIfInvalidAccount(newOverlay as Overlay));
 
+        /* Component Lifecycle */
         onBeforeUnmount(() => {
             if (!activeStake.value?.activeBalance && !activeStake.value?.inactiveBalance) {
                 useStakingStore().removeStake(activeAddressInfo.value!.address);
@@ -165,17 +185,27 @@ export default defineComponent({
         });
 
         return {
-            page,
-            overlay,
-            activeValidator,
-            adjustStake,
-            switchValidator,
-            closeOverlay,
-            invalidAccount,
-            isStaking,
+            // Enums
             Page,
             Overlay,
 
+            // General
+            page,
+            overlay,
+            closeOverlay,
+            shouldShowOverlay,
+
+            // Staking refs / computed
+            activeValidator,
+            invalidAccount,
+            isStaking,
+            selectedValidator,
+
+            // Staking methods / handlers
+            adjustStake,
+            switchValidator,
+            onSelectValidator,
+            onConfirmValidator,
             // StatusScreen
             statusType,
             statusState,
@@ -185,7 +215,6 @@ export default defineComponent({
             statusAlternativeAction,
             onStatusChange,
             StatusChangeType,
-            // onStatusMainAction,
         };
     },
     components: {
@@ -197,6 +226,7 @@ export default defineComponent({
         StakingRewardsHistoryPage,
         SelectAccountOverlay,
         StatusScreen,
+        ValidatorDetailsOverlay,
     },
 });
 </script>
@@ -204,7 +234,9 @@ export default defineComponent({
 <style lang="scss" scoped>
     .modal {
         ::v-deep .small-page {
-            transition: width 250ms ease;
+            transition:
+                width var(--transition-time) var(--nimiq-ease),
+                transform var(--transition-time) var(--nimiq-ease);
         }
 
         &.large-modal {
@@ -216,7 +248,7 @@ export default defineComponent({
 
     .fade-enter-active,
     .fade-leave-active {
-        transition: opacity 250ms ease;
+        transition: opacity 250ms var(--nimiq-ease);
     }
 
     .fade-enter,

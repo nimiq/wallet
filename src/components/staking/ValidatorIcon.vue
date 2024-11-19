6584068f0ded25c084ac17ef6b392ce049d49c97
Mraveux <matheo.raveux@gmail.com>
2024-11-19 01:28:30 -0600

    Add component ValidatorIcon

---
+++ src/components/staking/ValidatorIcon.vue
@@ -0,0 +1,31 @@
+<template>
+    <img class="validator-icon" v-if="'icon' in validator" :src="validator.icon" :alt="validator.name" />
+    <Identicon class="validator-icon" v-else :address="validator.address"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import { Identicon } from '@nimiq/vue-components';
+import { Validator } from '../../stores/Staking';
+
+export default defineComponent({
+    props: {
+        validator: {
+            type: Object as () => Validator,
+            required: true,
+        },
+    },
+    components: {
+        Identicon,
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+img, .identicon {
+    --size: 5.5rem;
+    width: var(--size);
+    height: var(--size);
+    display: block;
+}
+</style>

03953c88cc371c84b69eba16f0d1aba6bd0beab4
Sören <hello@soerenschwert.de>
2024-11-23 17:56:05 -0600

    Reduce Sentry sample rate in mainnet

--- src/lib/Sentry.ts
+++ src/lib/Sentry.ts
@@ -1,17 +1,18 @@
 import Vue from 'vue';
 import { captureException, init } from '@sentry/vue';
 import { useConfig } from '../composables/useConfig';
+import { ENV_MAIN } from './Constants';
 
 export function startSentry() {
     const { config } = useConfig();
     init({
-        // dsn: 'https://788f2bf1f3f74e408e50d2f2e5488e14@o208918.ingest.us.sentry.io/5289326',
         dsn: 'https://788f2bf1f3f74e408e50d2f2e5488e14@o208918.ingest.sentry.io/5289326',
         Vue,
         environment: config.environment,
         release: process.env.SENTRY_RELEASE,
         enabled: config.reportToSentry,
         logErrors: true,
+        sampleRate: config.environment === ENV_MAIN ? 0.2 : 1,
     });
 }
 

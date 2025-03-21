3f61ec55453f67b704cad6e5df83847a290870ca
Sören <hello@soerenschwert.de>
2025-03-21 18:34:48 +0100

    Update StakingOverview with unstaking and payout states

---
+++ src/components/icons/Staking/CircleExclamationMarkIcon.vue
@@ -0,0 +1,5 @@
+<template functional>
+    <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M16 8.5a8 8 0 1 1-16 0 8 8 0 0 1 16 0ZM7 4.25c0-.41.45-.75 1-.75s1 .34 1 .75v4.5c0 .41-.45.75-1 .75s-1-.34-1-.75v-4.5Zm1.02 8.79h.02a1.23 1.23 0 0 0 1.2-1.28 1.27 1.27 0 0 0-1.26-1.22h-.02a1.23 1.23 0 0 0-1.2 1.27c.02.68.58 1.22 1.26 1.23Z" fill="currentColor"/>
+    </svg>
+</template>

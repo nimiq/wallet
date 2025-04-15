f4830f29870efe6ee442c6c8f09f1bc2b1910430
onmax <maximogarciamtnez@gmail.com>
2025-04-15 10:15:56 +0200

    feat: improved demo fallback modal

--- src/components/modals/demos/DemoModalFallback.vue
+++ src/components/modals/demos/DemoModalFallback.vue
@@ -1,19 +1,20 @@
 <template>
   <Modal>
-    <PageHeader class="flex-column">
-      <h1 class="nq-h1">{{ $t('Nimiq Demo') }}</h1>
-    </PageHeader>
-    <PageBody>
+    <PageBody class="demo-modal-content">
+      <ThreeLoginFiles />
+
+      <h1 class="nq-h1">Get the real deal</h1>
+
       <p class="nq-p">
-        {{ $t('This is not a real Nimiq Wallet. It is just a demo so it is limited in functionality.') }}
+        {{ $t('You don\'t even need your email.') }}
       </p>
       <p>
-        {{ $t('You can open a free NIM account in less than a minute.') }}
+        {{ $t('Create a wallet simply by setting a password and give the real deal a try. No download, no costs, no personal data.') }}
       </p>
     </PageBody>
     <PageFooter>
-      <a href="https://wallet.nimiq.com" target="_blank" class="nq-button light-blue">
-        {{ $t('Open Nimiq Wallet') }}
+      <a href="https://wallet.nimiq.com" target="_blank" class="nq-button-pill light-blue">
+        {{ $t('Create a wallet') }}
       </a>
     </PageFooter>
   </Modal>
@@ -21,15 +22,47 @@
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
-import { PageBody, PageHeader, PageFooter } from '@nimiq/vue-components';
-import Modal from '../Modal.vue';
+import { PageBody, PageFooter } from '@nimiq/vue-components';
+import ThreeLoginFiles from '@/components/icons/ThreeLoginFiles.vue';
+import Modal from '@/components/modals/Modal.vue';
 
 export default defineComponent({
     components: {
         Modal,
-        PageHeader,
         PageBody,
+        ThreeLoginFiles,
         PageFooter,
     },
 });
 </script>
+
+<style lang="scss" scoped>
+.demo-modal-content {
+  text-align: center;
+  padding-top: 10rem;
+  padding-bottom: 0;
+
+  & ::v-deep svg {
+    max-width: 28rem;
+    margin: 0 auto;
+  }
+
+  & h1 {
+    margin-top: 5rem;
+  }
+
+  & p {
+    color: var(--text-50);
+    font-weight: 600;
+  }
+}
+
+.page-footer {
+  padding: 4rem;
+
+  & a {
+    width: max-content;
+    margin: 0 auto;
+  }
+}
+</style>

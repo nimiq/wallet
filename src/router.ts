7218bbcecd7300149be7798cb0ac538c7b5b3fc9
Daniel <github.danimoh@neverbox.com>
2025-09-29 15:20:25 +0200

    Staking events performance: remove unused/unreachable StakingRewardsHistoryModal/Page
    StakingRewardsHistoryModal had an entry in the router, but was from nowhere linked to.
    StakingRewardsHistoryPage was used in StakingRewardsHistoryModal and was declared as
    page and as overlay in StakingModal, but there was no code opening this page or overlay.
    The overlay was previously accessible via event "next" from StakingInfoPage, but commit
    "Add staking rewards to the staking modal" removed it without removing everything else...
    
    As these components are unreachable, it's not worth spending time on optimizing their
    performance, and instead we just remove them. Also, there would be plenty to do, because
    the performance is so bad that it kills the browser tab.

--- src/router.ts
+++ src/router.ts
@@ -90,11 +90,6 @@ const StakingModal = () =>
     import(/* webpackChunkName: "staking-modal" */ './components/staking/StakingModal.vue');
 const ValidatorDetailsModal = () =>
     import(/* webpackChunkName: "validator-details-modal" */ './components/staking/ValidatorDetailsModal.vue');
-const StakingRewardsHistoryModal = () =>
-    import(
-        /* webpackChunkName: "staking-rewards-history-modal" */
-        './components/staking/StakingRewardsHistoryModal.vue'
-    );
 const StakingRewardsModal = () =>
     import(/* webpackChunkName: "staking-rewards-modal" */ './components/staking/StakingRewardsModal.vue');
 
@@ -145,7 +140,6 @@ export enum RouteName {
     ExportHistory = 'export-history',
     Staking = 'staking',
     ValidatorDetails = 'validator-details',
-    StakingRewardsHistory = 'staking-rewards-history',
     WalletStatus = 'wallet-status',
     Settings = 'settings',
     SettingsAccounts = 'settings-accounts',
@@ -455,14 +449,6 @@ const routes: RouteConfig[] = [{
                 modal: ValidatorDetailsModal,
             },
             name: RouteName.ValidatorDetails,
-            props: { modal: true },
-            meta: { column: Columns.DYNAMIC },
-        }, {
-            path: '/staking-rewards-history/:month?',
-            components: {
-                modal: StakingRewardsHistoryModal,
-            },
-            name: RouteName.StakingRewardsHistory,
             props: { modal: true },
             meta: { column: Columns.DYNAMIC },
         }, {
